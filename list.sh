39a194d51396d1838d947222d676c2385f58bb00
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-13 01:37:08 +0800

    Create list.sh

---
+++ list.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Define the input file
+input_file="domains.txt"
+
+# Define a temporary file for storing the live domains
+temp_file=$(mktemp)
+
+# Initialize a counter for the number of removed domains
+removed_domains=0
+
+# Loop over each line in the input file
+while read -r domain; do
+  # Use dig to get the IP address for the domain
+  ip=$(dig +short "$domain")
+
+  # If the IP address is empty, the domain is considered dead
+  if [ -z "$ip" ]; then
+    echo "Removing dead domain: $domain"
+    removed_domains=$((removed_domains+1))
+  else
+    echo "$domain" >> "$temp_file"
+  fi
+done < "$input_file"
+
+# Copy the temporary file to the input file
+cp "$temp_file" "$input_file"
+
+# Sort the input file and overwrite it
+sort "$input_file" -o "$input_file"
+
+# Print the total number of removed domains
+echo "Total number of removed domains: $removed_domains"
+
+# Remove the temporary file
+rm "$temp_file"
