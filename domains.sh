41c258e20b775f0c00e7b7775d358cb2427e1a40
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-13 00:39:44 +0800

    Update domains.sh
    Added filter

--- domains.sh
+++ domains.sh
@@ -9,7 +9,7 @@ num_results=100
 
 search_url="https://www.google.com/search?q=${query}&num=${num_results}&filter=0"
 
-search_results=$(curl -s -A "$user_agent" "$search_url" | grep -o '<a href="[^"]*"' | sed 's/^<a href="//' | sed 's/"$//' | awk -F/ '{print $3}' | sort -u | sed 's/^www\.//' | grep -v -i 'scam' | grep -v -i 'google' | grep -v -i 'pinterest' | grep -v -i 'reddit' | grep -v -i 'zoominfo')
+search_results=$(curl -s -A "$user_agent" "$search_url" | grep -o '<a href="[^"]*"' | sed 's/^<a href="//' | sed 's/"$//' | awk -F/ '{print $3}' | sort -u | sed 's/^www\.//' | grep -v -i 'scam' | grep -v -i 'google' | grep -v -i 'pinterest' | grep -v -i 'reddit' | grep -v -i 'socialgrep' | grep -v -i 'zoominfo')
 
 for domain in $search_results; do
     if dig +short "$domain" | grep -q '^$'; then
