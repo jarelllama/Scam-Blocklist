7036d515051a8f494f0252d1271aee2f9f4bf0db
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-13 00:12:43 +0800

    Create domains.sh

---
+++ domains.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read -p "Enter a search query: " og_query
+
+query="\"$og_query\""
+query=$(echo "$query" | sed 's/ /+/g')
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+num_results=100
+
+search_url="https://www.google.com/search?q=${query}&num=${num_results}&filter=0"
+
+search_results=$(curl -s -A "$user_agent" "$search_url" | grep -o '<a href="[^"]*"' | sed 's/^<a href="//' | sed 's/"$//' | awk -F/ '{print $3}' | sort -u | sed 's/^www\.//' | grep -v -i 'scam' | grep -v -E '^google\.com$|^.*\.google\.com$' | grep -v -E '^reddit\.com$|^.*\.reddit\.com$')
+
+for domain in $search_results; do
+    if dig +short "$domain" | grep -q '^$'; then
+        continue
+    fi
+    echo "$domain"
+done
+
+echo "Search term used: $og_query"
