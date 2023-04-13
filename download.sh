7b91c14c5776c06255db4fa43aa224f4d4e2f86f
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-13 12:37:03 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download whitelist.txt from GitHub
+wget https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/whitelist.txt
+
+# Download toplist.txt from GitHub
+wget https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt
