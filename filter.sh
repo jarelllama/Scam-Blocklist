b28e17410790d1bb4df7bb8f1b264a8fa8fd69d0
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-14 05:04:28 +0800

    Create filter.sh

---
+++ filter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+new_domains_file="new_domains.txt"
+whitelist_file="whitelist.txt"
+
+# Count number of lines in original file
+original_count=$(wc -l < "$new_domains_file")
+
+# Remove duplicates and domains matching whitelist
+sort -uf "$new_domains_file" | comm -23 - <(sort -f "$whitelist_file") > "$new_domains_file.tmp"
+mv "$new_domains_file.tmp" "$new_domains_file"
+
+# Sort final list alphabetically
+sort -f "$new_domains_file" -o "$new_domains_file"
+
+# Print removed domains and reasons
+echo "Removed domains:"
+awk -F. '{print tolower($1)}' "$new_domains_file" | sort | uniq -d | grep -wf - <(sort -f "$new_domains_file" "$whitelist_file" | uniq -d) | sed 's/^\([^[:space:]]*\)/\1 (duplicate)/'
+grep -wif "$whitelist_file" "$new_domains_file" | sed 's/^\([^[:space:]]*\)/\1 (whitelisted)/'
+
+# Count number of lines in final file
+total_count=$(wc -l < "$new_domains_file")
+
+# Print results
+echo "Original number of domains: $original_count"
+echo "Total domains removed: $(($original_count - $total_count))"
+echo "Final number of domains: $total_count"
