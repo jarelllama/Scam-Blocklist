4a098da5593685fde3768403da1345e6c41070f4
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-13 10:25:55 +0800

    Check for NXDOMAINS

--- list.sh
+++ list.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Define the input file
-input_file="domains.txt"
+# Define the input file                              input_file="domains.txt"
 
-# Define a temporary file for storing the unique, live domains
+# Define a temporary file for storing the live domains
 temp_file=$(mktemp)
 
 # Initialize a counter for the number of removed domains
@@ -11,18 +10,11 @@ removed_domains=0
 
 # Loop over each line in the input file
 while read -r domain; do
-  # Use dig to get the IP address for the domain
-  ip=$(dig +short "$domain")
-  
-  # If the IP address is empty, the domain is considered dead
-  if [ -z "$ip" ]; then
-    echo "Removing dead domain: $domain"
-    removed_domains=$((removed_domains+1))
+  # If the domain returns NXDOMAIN, remove it
+  if dig @1.1.1.1 "$domain" | grep -q 'NXDOMAIN'; then
+    echo "Removing dead domain: $domain"                 removed_domains=$((removed_domains+1))
   else
-    # Check if the domain is already in the temporary file
-    if ! grep -qFx "$domain" "$temp_file"; then
-      echo "$domain" >> "$temp_file"
-    fi
+    echo "$domain" >> "$temp_file"
   fi
 done < "$input_file"
 
@@ -30,7 +22,7 @@ done < "$input_file"
 cp "$temp_file" "$input_file"
 
 # Sort the input file and overwrite it
-sort -u "$input_file" -o "$input_file"
+sort "$input_file" -o "$input_file"
 
 # Print the total number of removed domains
 echo "Total number of removed domains: $removed_domains"
