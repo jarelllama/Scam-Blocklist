c5b801201eb0ea5f82fbda66436eb128a5bb4a87
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-13 13:40:21 +0800

    added option for toplist

--- download.sh
+++ download.sh
@@ -2,6 +2,12 @@
 
 # Download required files
 
+read -p "Do you want to download the toplist.txt? (y/n)" choice
+
+if [ $choice == "y" ]; then
+wget -N https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt
+fi
+
 wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/domains.sh
 
 wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/list.sh
