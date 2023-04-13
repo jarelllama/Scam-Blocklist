274b4cea7eb45f05a77814a55e63ff42c36b5373
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-14 02:11:11 +0800

    script cosmetic change

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@
 read -p "Do you want to download the toplist.txt? (y/N)" choice
 
 if [ "$choice" == "y" ]; then
-wget -N https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt
+  wget -N https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt
 fi
 
 wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/domains.sh
