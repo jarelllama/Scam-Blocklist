d49fdf20d5d3bed4f238ab45d1a31f57920af882
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-13 12:49:50 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Download whitelist.txt from GitHub
+# Download required files
+
+wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/domains.sh
+
+wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/list.sh
+
 wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/whitelist.txt
 
-# Download toplist.txt from GitHub
 wget -N https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt
