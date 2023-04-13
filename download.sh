02fb468f53e369762cc6cfad539a9edd4a5aa19e
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-13 13:40:43 +0800

    fixes

--- download.sh
+++ download.sh
@@ -15,5 +15,3 @@ wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/list.sh
 wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/blacklist.txt
 
 wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/whitelist.txt
-
-wget -N https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt
