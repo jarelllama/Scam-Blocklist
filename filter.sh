55377cdcea73c268a4263d1e3e38a304e8263966
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-16 03:41:00 +0800

    Add whitelist filter

--- filter.sh
+++ filter.sh
@@ -3,6 +3,7 @@
 # Define input and output file locations
 input_file="pending_domains.txt"
 output_file="filtered_domains.txt"
+whitelist_file="whitelist.txt"
 
 # Remove empty lines and duplicates
 awk '!a[$0]++ && NF' "$input_file" > "tmp1.txt"
@@ -10,6 +11,12 @@ awk '!a[$0]++ && NF' "$input_file" > "tmp1.txt"
 # Find and print out duplicated domains
 awk 'seen[$0]++ == 1 { print $0 , "(duplicate)" }' "$input_file"
 
+# Remove whitelisted domains
+awk 'FNR==NR{a[tolower($1)]++; next} !a[tolower($1)]' "$whitelist_file" "tmp1.txt" | grep -vf "$whitelist_file" -i | cut -d' ' -f1 > "tmp2.txt"
+
+# Print whitelisted domains
+grep -f "$whitelist_file" -i "tmp1.txt" | cut -d' ' -f1 | while read line; do echo "$line (whitelisted)"; done
+
 # Move the temporary file to the desired output file
-mv "tmp1.txt" "$output_file"
+mv "tmp2.txt" "$output_file"
 
