86cdfa9307c495124ffd96c7bcef4d1acb04ea29
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-16 04:09:03 +0800

    Add toplist comparison

--- filter.sh
+++ filter.sh
@@ -4,6 +4,8 @@
 input_file="pending_domains.txt"
 output_file="filtered_domains.txt"
 whitelist_file="whitelist.txt"
+blacklist_file="blacklist.txt"
+toplist_file="toplist.txt"
 
 echo "Domains removed:"
 
@@ -11,7 +13,7 @@ echo "Domains removed:"
 awk '!a[$0]++ && NF' "$input_file" > "tmp1.txt"
 
 # Find and print out duplicated domains
-awk 'seen[$0]++ == 1 { print $0 , "(duplicate)" }' "$input_file"
+awk 'seen[$0]++ == 1 { print $0 , "(duplicate)" }' "tmp1.txt"
 
 # Remove whitelisted domains
 awk -v FS=" " 'FNR==NR{a[tolower($1)]++; next} !a[tolower($1)]' "$whitelist_file" "tmp1.txt" | grep -vf "$whitelist_file" -i | awk -v FS=" " '{print $1}' > "tmp2.txt"
@@ -22,3 +24,6 @@ grep -f "$whitelist_file" -i "tmp1.txt" | awk '{print $1" (whitelisted)"}'
 # Move the temporary file to the desired output file
 mv "tmp2.txt" "$output_file"
 
+# Compare with toplist domains
+echo "Domains in toplist:"
+comm -12 <(sort "$output_file") <(sort "$toplist_file") | grep -vFxf "$blacklist_file"
