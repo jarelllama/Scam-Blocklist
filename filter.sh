bbd3ec1144ddf773cb39dc6b767c1bb4d6abb997
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-16 01:42:43 +0800

    Update

--- filter.sh
+++ filter.sh
@@ -4,13 +4,12 @@
 input_file="pending_domains.txt"
 output_file="filtered_domains.txt"
 
-# Use awk to remove empty lines and duplicates
+# Remove empty lines and duplicates
 awk '!a[$0]++ && NF' "$input_file" > "tmp1.txt"
 
-# Find and print out any duplicated domains
+# Find and print out duplicated domains
 sort "$input_file" | uniq -d | while read -r line; do printf "%s (duplicate)\n" "$line"; done
 
-# Move the temporary file to the desired output file name
+# Move the temporary file to the desired output file
 mv "tmp1.txt" "$output_file"
 
-# Rest of the code goes here...
