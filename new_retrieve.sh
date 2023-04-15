714535b52da090549ed5179c3567b603c6c6e83c
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-15 20:11:58 +0800

    Cosmetic changes for console

--- new_retrieve.sh
+++ new_retrieve.sh
@@ -5,11 +5,13 @@ whitelist_file="whitelist.txt"
 new_domains_file="new_domains.txt"
 search_terms_file="search_terms.txt"
 
-# Define the number of search results per page
+# Define the number of search results
 num_results=120
 
 # Define a user agent to prevent Google from blocking the search request
 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+
+echo "Search terms:"
 
 # Read search terms from file and loop through each term
 while IFS= read -r line || [[ -n "$line" ]]; do
@@ -18,14 +20,11 @@ while IFS= read -r line || [[ -n "$line" ]]; do
         # Encode the search term for use in the Google search URL
         encoded_search_term=${line// /+}
 
-        # Use the search term to search Google with filtering off and get up to 120 search results per page
+        # Use the search term to search Google with filtering off
         google_search_url="https://www.google.com/search?q=\"${encoded_search_term}\"&num=$num_results&filter=0"
 
-        # Send the request to Google and extract all domains and subdomains from the HTML, then remove www. and empty lines
+        # Send the request to Google and extract all domains and subdomains from the HTML. Remove www., empty lines and duplicates
         domains=$(curl -s --max-redirs 0 -H "User-Agent: $user_agent" "$google_search_url" | grep -o '<a href="[^"]*"' | sed 's/^<a href="//' | sed 's/"$//' | awk -F/ '{print $3}' | sort -u | sed 's/^www\.//' | sed '/^$/d')
-
-        # Remove empty lines from domains
-        #domains=$(echo -e "${domains}" | sed '/^\s*$/d')
 
         # Count the number of domains
         if [[ -z "$domains" ]]; then
@@ -35,7 +34,8 @@ while IFS= read -r line || [[ -n "$line" ]]; do
         fi
 
         # Print the total number of domains for each search term
-        echo "Search term: $line"
-        echo "Total number of domains: $num_domains"
+        echo "$line"
+        echo "Number of domains found: $num_domains"
+        echo "----------------------------------"
     fi
 done < "$search_terms_file"
