b1e59337bb30bdc5e1ad583ce520906bd66a3089
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-16 02:11:32 +0800

    Optimizations

--- filter.sh
+++ filter.sh
@@ -8,7 +8,7 @@ output_file="filtered_domains.txt"
 awk '!a[$0]++ && NF' "$input_file" > "tmp1.txt"
 
 # Find and print out duplicated domains
-sort "$input_file" | uniq -d | while read -r line; do printf "%s (duplicate)\n" "$line"; done
+awk 'seen[$0]++ == 1 { print $0 , "(duplicate)" }' "$input_file"
 
 # Move the temporary file to the desired output file
 mv "tmp1.txt" "$output_file"
