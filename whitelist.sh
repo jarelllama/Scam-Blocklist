7bf63b18b9387bb4960d133718ba9ab435824450
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-16 17:39:01 +0800

    Update whitelist.sh

--- whitelist.sh
+++ whitelist.sh
@@ -2,6 +2,7 @@
 
 whitelist_file="whitelist.txt"
 
-# Prompt user to enter a new entry to the whitelist
-read -p "Enter a new entry : " new_entry
-new_entry=$(echo "${new_entry}" | tr '[:upper:]' '[:lower:]')
+read -rp "Enter the new entry: " new_entry
+new_entry="${new_entry,,}"
+
+echo "$new_entry" >> "$whitelist_file"
