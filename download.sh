890b359edfaaff24f7a0d1cbd48e75e6a01045ba
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-16 15:06:36 +0800

    update

--- download.sh
+++ download.sh
@@ -4,13 +4,13 @@
 
 read -p "Do you want to download the toplist.txt? (y/N)" choice
 
-if [ "$choice" == "y" ]; then
+if [[ "$choice" == "y" ]]; then
   wget -N https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt
 fi
 
-wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/domains.sh
+wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/retrieve.sh
 
-wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/list.sh
+wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/merge.sh
 
 wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/blacklist.txt
 
