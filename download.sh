1a416f8765a5a5d400b61359c9792b550e8e5945
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-18 07:16:02 +0800

    replaced add.sh with edit.sh

--- download.sh
+++ download.sh
@@ -18,4 +18,4 @@ wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/white_t
 
 wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/search_terms.txt
 
-wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/add.sh
+wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/edit.sh
