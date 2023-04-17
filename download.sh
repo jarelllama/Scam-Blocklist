18e1f23116d39a8925fd8cb90e655dbe901e653e
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-17 23:11:32 +0800

    Removed/added required files

--- download.sh
+++ download.sh
@@ -8,13 +8,13 @@ if [[ "$answer" == "y" ]]; then
   wget -N https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt
 fi
 
-wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/retrieve.sh
-
-wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/merge.sh
+wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/update.sh
 
 wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/blacklist.txt
 
 wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/whitelist.txt
+
+wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/white_tlds.txt
 
 wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/search_terms.txt
 
