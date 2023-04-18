40dd2b747ce345fe4a02ccb4aa4015452aea7940
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-18 13:57:38 +0800

    Added option to remove entry

--- edit.sh
+++ edit.sh
@@ -19,22 +19,27 @@ while true; do
     case "$choice" in
         1)
             echo "Blocklist"
-            # Add the new entry if the domain isn't already in the blocklist
-            if grep -q "^$new_entry$" "$domains_file"; then
-                echo "The domain is already in the blocklist"
+
+            # Remove domain from the blocklist
+            if [[ $new_entry == -* ]]; then
+                domain=$(echo "$new_entry" | cut -c 2-)
+                sed -i "/^$domain$/d" "$domains_file"
             else
-                # Check if domain is dead
+                # Add the new entry if the domain isn't already in the blocklist
+                if grep -q "^$new_entry$" "$domains_file"; then
+                    echo "The domain is already in the blocklist"
+                else
+                    echo "$new_entry" >> "$domains_file"
 
-                echo "$new_entry" >> "$domains_file"
+                    # Remove empty lines
+                    awk NF "$domains_file" > tmp1.txt
 
-                # Remove empty lines
-                awk NF "$domains_file" > tmp1.txt
+                    # Sort alphabetically
+                    sort -o "$domains_file" tmp1.txt
 
-                # Sort alphabetically
-                sort -o "$domains_file" tmp1.txt
-
-                # Remove temporary file
-                rm tmp1.txt
+                    # Remove temporary file
+                    rm tmp1.txt
+                fi
             fi
 
             # Go back to options prompt
@@ -42,21 +47,28 @@ while true; do
             ;;
         2)
             echo "Whitelist"
-            # Add the new entry if a similar term isn't already in the whitelist
-            if grep -Fiq "$new_entry" "$whitelist_file"; then
-                existing_entry=$(grep -Fi "$new_entry" "$whitelist_file" | head -n 1)
-                echo "A similar term is already in the whitelist: $existing_entry"
+
+            # Remove term from the whitelist
+            if [[ $new_entry == -* ]]; then
+                term=$(echo "$new_entry" | cut -c 2-)
+                sed -i "/$term/d" "$whitelist_file"
             else
-                echo "$new_entry" >> "$whitelist_file"
+                # Add the new entry if a similar term isn't already in the whitelist
+                if grep -Fiq "$new_entry" "$whitelist_file"; then
+                    existing_entry=$(grep -Fi "$new_entry" "$whitelist_file" | head -n 1)
+                    echo "A similar term is already in the whitelist: $existing_entry"
+                else
+                    echo "$new_entry" >> "$whitelist_file"
 
-                # Remove empty lines
-                awk NF "$whitelist_file" > tmp1.txt
+                    # Remove empty lines
+                    awk NF "$whitelist_file" > tmp1.txt
 
-                # Sort alphabetically
-                sort -o "$whitelist_file" tmp1.txt
+                    # Sort alphabetically
+                    sort -o "$whitelist_file" tmp1.txt
 
-                # Remove temporary file
-                rm tmp1.txt
+                    # Remove temporary file
+                    rm tmp1.txt
+                fi
             fi
 
             # Go back to options prompt
@@ -64,20 +76,27 @@ while true; do
             ;;
         3)
             echo "Blacklist"
-            # Add the new entry if the domain isn't already in the blacklist
-            if grep -q "^$new_entry$" "$blacklist_file"; then
-                echo "The domain is already in the blacklist"
+
+            # Remove domain from the blacklist
+            if [[ $new_entry == -* ]]; then
+                domain=$(echo "$new_entry" | cut -c 2-)
+                sed -i "/^$domain$/d" "$blacklist_file"
             else
-                echo "$new_entry" >> "$blacklist_file"
+                # Add the new entry if the domain isn't already in the blacklist
+                if grep -q "^$new_entry$" "$blacklist_file"; then
+                    echo "The domain is already in the blacklist"
+                else
+                    echo "$new_entry" >> "$blacklist_file"
 
-                # Remove empty lines
-                awk NF "$blacklist_file" > tmp1.txt
+                    # Remove empty lines
+                    awk NF "$blacklist_file" > tmp1.txt
 
-                # Sort alphabetically
-                sort -o "$blacklist_file" tmp1.txt
+                    # Sort alphabetically
+                    sort -o "$blacklist_file" tmp1.txt
 
-                # Remove temporary file
-                rm tmp1.txt
+                    # Remove temporary file
+                    rm tmp1.txt
+                fi
             fi
 
             # Go back to options prompt
