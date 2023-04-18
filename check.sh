9a3898b2fd914983736d00d42dd72b4abd32f4c1
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-18 15:00:42 +0800

    Placeholder

---
+++ check.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+domains_file="domains.txt"
+whitelist_file="whitelist.txt"
+blacklist_file="blacklist.txt"
+toplist_file="toplist.txt"
+tlds_file="white_tlds.txt"
+
+    # Backup the domains file before making any changes
+    cp "$domains_file" "$domains_file.bak"
+
+    # Create temporary file
+    touch tmp1.txt
+
+# Count the number of domains before filtering
+    num_before=$(wc -l < "$domains_file")
+
+    # Remove duplicates and sort alphabetically
+    sort -u -o "$domains_file" "$domains_file"
+
+    echo "Domains removed:"
+
+    # Print and remove non domain entries
+    awk '{ if ($0 ~ /^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$/) print $0 > "tmp1.txt"; else print $0" (invalid)" }' "$domains_file"
+
+    # Print whitelisted domains
+    grep -f "$whitelist_file" -i tmp1.txt | awk '{print $1" (whitelisted)"}'
+
+    # Remove whitelisted domains
+    awk -v FS=" " 'FNR==NR{a[tolower($1)]++; next} !a[tolower($1)]' "$whitelist_file" tmp1.txt | grep -vf "$whitelist_file" -i | awk -v FS=" " '{print $1}' > tmp2.txt
+
+    # Print domains with whitelisted TLDs
+    grep -oE "(\S+)\.($(paste -sd '|' "$tlds_file"))$" tmp2.txt | sed "s/\(.*\)/\1 (TLD)/"
+
+    # Remove domains with whitelisted TLDs
+    grep -vE "\.($(paste -sd '|' "$tlds_file"))$" tmp2.txt > tmp3.txt
+
+    # Remove www subdomains
+    sed -i 's/^www\.//' tmp3.txt
+
+    # Save changes to the domains file
+    mv tmp3.txt "$domains_file"
+
+    # Print domains found in the toplist
+    echo -e "\nDomains in toplist:"
+    comm -12 "$domains_file" <(sort "$toplist_file") | grep -vFxf "$blacklist_file"
+
+    # Count the number of domains after filtering
+    num_after=$(wc -l < "$domains_file")
+
+    # Remove temporary files
+    rm tmp*.txt
+
+    # Print counters
+    echo "Total domains before: $num_before"
+    echo "Total domains removed: $((num_before - num_after))"
+    echo "Final domains after: $num_after"
+}
