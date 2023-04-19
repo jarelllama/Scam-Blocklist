9c8e6cecfb5bd05ae106f99da59c32176ad202da
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-19 22:10:41 +0800

    Optimized grep and reduced comments

--- update.sh
+++ update.sh
@@ -19,7 +19,7 @@ declare -A retrieved_domains
 
 echo "Search terms:"
 
-# Loop through each search term in its entirety from the search terms file
+# A blank IFS ensures the entire search term is read
 while IFS= read -r term; do
     if [[ -n "$term" ]]; then
         # gsub is used here to replace consecutive non-alphanumeric characters with a single plus sign
@@ -64,24 +64,22 @@ function filter_pending {
     # Although the retrieved domains are already deduplicated, not emptying the pending domains file may result in duplicates
     sort -u tmp3.txt -o tmp4.txt
 
-    # Keep only pending domains not already in the blocklist for filtering
     # This removes the majority of pending domains and makes the further filtering more efficient
     comm -23 tmp4.txt "$domains_file" > tmp5.txt
 
     echo "Domains removed:"
 
-    grep -f "$whitelist_file" tmp5.txt | awk '{print $1" (whitelisted)"}'
+    grep -Ff "$whitelist_file" tmp5.txt | awk '{print $0 " (whitelisted)"}'
 
-    grep -vf "$whitelist_file" tmp5.txt > tmp6.txt
+    grep -vFf "$whitelist_file" tmp5.txt > tmp6.txt
 
     # Print and remove non domain entries
     # The regex checks for one or more alphanumeric characters, periods or dashes infront of a period followed by two or more alphanumeric characters
-    awk '{ if ($0 ~ /^[[:alnum:].-]+\.[[:alnum:]]{2,}$/) print $0 > "tmp7.txt"; else print $0" (invalid)" }' tmp6.txt
+    awk '{ if ($0 ~ /^[[:alnum:].-]+\.[[:alnum:]]{2,}$/) print $0 > "tmp7.txt"; else print $0 " (invalid)" }' tmp6.txt
 
-    # Print domains with whitelisted TLDs
-    grep -E "(\S+)\.($(paste -sd '|' "$tlds_file"))$" tmp7.txt | awk '{print $1" (TLD)"}'
+    # The regex finds entries with whitelisted TLDs
+    grep -E "(\S+)\.($(paste -sd '|' "$tlds_file"))$" tmp7.txt | awk '{print $0 " (TLD)"}'
 
-    # Remove domains with whitelisted TLDs
     grep -vE "\.($(paste -sd '|' "$tlds_file"))$" tmp7.txt > tmp8.txt
 
     touch tmp_dead.txt
@@ -101,7 +99,7 @@ function filter_pending {
 
     # Check if the www subdomains are resolving
     cat tmpA.txt | xargs -I{} -P4 bash -c "
-        if ! dig @1.1.1.1 {} | grep -q 'NXDOMAIN'; then
+        if ! dig @1.1.1.1 {} | grep -Fq 'NXDOMAIN'; then
             echo {} >> tmp_www.txt
             echo '{} is resolving'
         fi
@@ -110,59 +108,44 @@ function filter_pending {
     comm -23 <(sort tmp_www.txt) tmp9.txt >> tmp10.txt
 
     sort tmp10.txt -o "$pending_file"
-    
-    # TODO...
-    
-    num_pending=$(wc -l < "$pending_file")
 
-    # Remove temporary files
     rm tmp*.txt
 
-    # Print counters
     echo -e "\nTotal domains retrieved: $num_retrieved"
-    echo "Domains not in blocklist: $num_pending"
+    echo "Domains not in blocklist: $(wc -l < "$pending_file")"
     echo "Domains:"
     cat "$pending_file"
     echo -e "\nDomains in toplist:"
+    # About 8x faster than comm due to not needing to sort the toplist
+    # Note that www subdomains arent matched
     grep -xFf "$pending_file" "$toplist_file" | grep -vxFf "$blacklist_file"
 }
 
-# Execute filtering for pending domains
 filter_pending
 
-# Define a function to merge filtered pending domains to the domains file
 function merge_pending {
     echo "Merge with blocklist"
 
-    # Backup the domains file before making any changes
     cp "$domains_file" "$domains_file.bak"
 
-    # Count the number of domains before merging
     num_before=$(wc -l < "$domains_file")
 
-    # Append unique pending domains to the domains file
     comm -23 "$pending_file" "$domains_file" >> "$domains_file"
 
-    # Sort alphabetically
-    sort -o "$domains_file" "$domains_file"
+    sort "$domains_file" -o "$domains_file" 
 
-    # Count the number of domains after merging
     num_after=$(wc -l < "$domains_file")
 
-    # Print counters
     echo "--------------------------------------------"
     echo "Total domains before: $num_before"
     echo "Total domains added: $((num_after - num_before))"
     echo "Final domains after: $num_after"
 
-    # Empty pending domains file
     > "$pending_file"
 
-    # Exit script
     exit 0
 }
 
-# Prompt the user with options on how to proceed
 while true; do
     echo -e "\nChoose how to proceed:"
     echo "1. Merge with blocklist (default)"
@@ -183,58 +166,47 @@ while true; do
             # Change the new entry to lowecase
             new_entry="${new_entry,,}"
 
-            # Check if a similar term is already in the whitelist
-            if grep -Fiq "$new_entry" "$whitelist_file"; then
-                existing_entry=$(grep -Fi "$new_entry" "$whitelist_file" | head -n 1)
+            if grep -qF "$new_entry" "$whitelist_file"; then
+                # head -n is used here for when multiple whitelisted terms match the new entry
+                existing_entry=$(grep -F "$new_entry" "$whitelist_file" | head -n 1)
                 echo "A similar term is already in the whitelist: $existing_entry"
                 continue
             fi
 
-            # Add the new entry
             echo -e "\nAdded to whitelist: $new_entry"
             echo "$new_entry" >> "$whitelist_file"
 
-            # Remove empty lines
             awk NF "$whitelist_file" > tmp1.txt
 
-            # Save changes and sort alphabetically
-            sort -o "$whitelist_file" tmp1.txt
+            sort tmp1.txt -o "$whitelist_file"
 
-            # Remove temporary file
-            rm tmp1.txt
+            rm tmp*.txt
             continue
             ;;
         3)
             echo "Add to blacklist"
             read -p "Enter the new entry: " new_entry
-            
-            # Change the new entry to lowecase
+
             new_entry="${new_entry,,}"
             
-            # Check if the entry is valid
-            if ! [[ $new_entry =~ ^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$ ]]; then
+            if ! [[ $new_entry =~ ^[[:alnum:].-]+\.[[:alnum:]]{2,}$ ]]; then
                 echo -e "\nInvalid entry."
                 continue
             fi
 
-            # Check if the new entry is already in the list
-            if grep -xq "$new_entry" "$blacklist_file"; then
+            if grep -xFq "$new_entry" "$blacklist_file"; then
                 echo "The domain is already in the blacklist. Not added."
                 continue
             fi
 
-            # Add the new entry
             echo -e "\nAdded to blacklist: $new_entry"
             echo "$new_entry" >> "$blacklist_file"
 
-            # Remove empty lines
             awk NF "$blacklist_file" > tmp1.txt
 
-            # Save changes and sort alphabetically
-            sort -o "$blacklist_file" tmp1.txt
+            sort tmp1.txt -o "$blacklist_file" 
 
-            # Remove temporary file
-            rm tmp1.txt
+            rm tmp*.txt
             continue
             ;;
         4)
@@ -246,13 +218,11 @@ while true; do
             exit 0
             ;;
         *)
-            # Use domain merger as the default option
+            # The z flag checks if the variable is empty
             if [[ -z "$choice" ]]; then
                 merge_pending
             else
                 echo "Invalid option."
-
-                # Go back to options prompt
                 continue     
             fi
     esac
