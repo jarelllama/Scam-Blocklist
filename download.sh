e648c9cc4aec009eb01eab7d592ffac74c42215b
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-20 16:21:21 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-#read -p "Download the toplist.txt? (y/N): " answer
-
-#if [[ "$answer" == "y" ]]; then
-  wget -N https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt
-#fi
-
-#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/update.sh
-
-#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/blacklist.txt
-
-#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/whitelist.txt
-
-#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/white_tlds.txt
-
-#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/search_terms.txt
-
-#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/check.sh
-
-#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/edit.sh
+wget -N https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt
