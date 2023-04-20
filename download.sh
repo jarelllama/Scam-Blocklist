2241c8ee8ae47516102e71009727f2251581e321
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-20 15:33:03 +0800

    Added toplist

--- download.sh
+++ download.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-read -p "Download the toplist.txt? (y/N): " answer
+#read -p "Download the toplist.txt? (y/N): " answer
 
-if [[ "$answer" == "y" ]]; then
+#if [[ "$answer" == "y" ]]; then
   wget -N https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt
-fi
+#fi
 
-wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/update.sh
+#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/update.sh
 
-wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/blacklist.txt
+#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/blacklist.txt
 
-wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/whitelist.txt
+#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/whitelist.txt
 
-wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/white_tlds.txt
+#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/white_tlds.txt
 
-wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/search_terms.txt
+#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/search_terms.txt
 
-wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/check.sh
+#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/check.sh
 
-wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/edit.sh
+#wget -N https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/edit.sh
