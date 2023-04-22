c5fde45ea8d7572698afae0a4c505569da456ffd
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-22 22:54:29 +0800

    Update blocklist header after running

--- check.sh
+++ check.sh
@@ -7,50 +7,64 @@ toplist_file="toplist.txt"
 
 cp "$domains_file" "$domains_file.bak"
 
-awk NF "$domains_file" > tmp1.txt
+# Strip the blocklist header (title, description, homepage, etc.)
+tail -n +9 "$domains_file" > tmp1.txt
 
-tr '[:upper:]' '[:lower:]' < tmp1.txt > tmp2.txt
+awk NF tmp1.txt > tmp2.txt
 
-num_before=$(wc -l < tmp2.txt)
+tr '[:upper:]' '[:lower:]' < tmp2.txt > tmp3.txt
 
-sort -u tmp2.txt -o tmp3.txt
+num_before=$(wc -l < tmp3.txt)
+
+sort -u tmp3.txt -o tmp4.txt
 
 echo "Domains removed:"
 
-grep -Ff "$whitelist_file" tmp3.txt | grep -vxFf "$blacklist_file" | awk '{print $0 " (whitelisted)"}'
+grep -Ff "$whitelist_file" tmp4.txt | grep -vxFf "$blacklist_file" | awk '{print $0 " (whitelisted)"}'
 
-grep -Ff "$whitelist_file" tmp3.txt | grep -vxFf "$blacklist_file" > tmp_white.txt
+grep -Ff "$whitelist_file" tmp4.txt | grep -vxFf "$blacklist_file" > tmp_white.txt
 
-comm -23 tmp3.txt <(sort tmp_white.txt) > tmp4.txt
+comm -23 tmp4.txt <(sort tmp_white.txt) > tmp5.txt
 
-grep -E '\.(edu|gov)$' tmp4.txt | awk '{print $0 " (TLD)"}'
+grep -E '\.(edu|gov)$' tmp5.txt | awk '{print $0 " (TLD)"}'
 
-grep -vE '\.(edu|gov)$' tmp4.txt > tmp5.txt
+grep -vE '\.(edu|gov)$' tmp5.txt > tmp6.txt
 
-grep -vE '^[[:alnum:].-]+\.[[:alnum:]]{2,}$' tmp5.txt | awk '{print $0 " (invalid)"}'
+grep -vE '^[[:alnum:].-]+\.[[:alnum:]]{2,}$' tmp6.txt | awk '{print $0 " (invalid)"}'
     
-grep -E '^[[:alnum:].-]+\.[[:alnum:]]{2,}$' tmp5.txt > tmp6.txt
+grep -E '^[[:alnum:].-]+\.[[:alnum:]]{2,}$' tmp6.txt > tmp7.txt
 
 touch tmp_dead.txt
 
-cat tmp6.txt | xargs -I{} -P8 bash -c "
+cat tmp7.txt | xargs -I{} -P8 bash -c "
   if dig @1.1.1.1 {} | grep -Fq 'NXDOMAIN'; then
       echo {} >> tmp_dead.txt
       echo '{} (dead)'
   fi
 "
 
-comm -23 tmp6.txt <(sort tmp_dead.txt) > tmp7.txt
+comm -23 tmp7.txt <(sort tmp_dead.txt) > tmp8.txt
 
-mv tmp7.txt "$domains_file"
+mv tmp8.txt "$domains_file"
 
 echo -e "\nDomains in toplist:"
 grep -xFf "$domains_file" "$toplist_file" | grep -vxFf "$blacklist_file"
 
 num_after=$(wc -l < "$domains_file")
 
-rm tmp*.txt
-
 echo "Total domains before: $num_before"
 echo "Total domains removed: $((num_before - num_after))"
 echo "Final domains after: $num_after"
+
+echo "# Title: Jarelllama's Scam Blocklist
+# Description: Blocklist for scam sites extracted from Google
+# Homepage: https://github.com/jarelllama/Scam-Blocklist
+# Source: https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/domains
+# License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+# Last modified: $(date -u)
+# Total number of domains: $(num_after)
+" | cat - "$domains_file" > tmp1.txt
+
+mv tmp1.txt "$domains_file"
+
+rm tmp*.txt
