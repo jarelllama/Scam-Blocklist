1144135eb7da20604b473ae1ce406301f8a2ebdf
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-22 20:14:00 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 domains_file="domains"
 
-sed -i "s/Current number of domains: .*/Current number of domains: $(wc -l < "$domains_file")/" README.md
+sed -i "s/Current number of domains: .*/Current number of domains: \`$(wc -l < "$domains_file")\`/" README.md
 
 git add "$domains_file" README.md
 git commit -m "Update domains"
