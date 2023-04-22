619eda64633e1d075f096437181868cca8f7c7fb
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-22 18:06:47 +0800

    Update toplist.sh

--- toplist.sh
+++ toplist.sh
@@ -5,5 +5,5 @@ toplist_file="toplist.txt"
 wget -N https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt -O "$toplist_file"
 
 git add "$toplist_file"
-git commit -m "Update toplist.txt"
+git commit -m "Update $toplist_file"
 git push
