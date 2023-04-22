34e15d3b0430a67b4e0627b751d3e6b3600d8767
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-22 14:02:03 +0800

    Check for www subdomains when adding to blacklist

--- edit.sh
+++ edit.sh
@@ -5,27 +5,6 @@ whitelist_file="whitelist.txt"
 blacklist_file="blacklist.txt"
 toplist_file="toplist.txt"
 
-function remove_entry {
-    # Remove the minus sign
-    new_entry=$(echo "$new_entry" | cut -c 2-)
-
-    if grep -xFq "$new_entry" "$2"; then
-        echo -e "\nRemoved from $1: $new_entry"
-        sed -i "/^$new_entry$/d" "$2"
-    else
-        echo -e "\nEntry not found in $1: $new_entry"
-    fi
-}
-
-function add_entry {
-    echo -e "\nAdded to $1: $new_entry"
-    echo "$new_entry" >> "$2"
-
-    awk NF "$2" > tmp1.txt
-    sort tmp1.txt -o "$2" 
-    rm tmp*.txt
-}
-
 function edit_blocklist {
     echo "Blocklist"
 
@@ -129,7 +108,13 @@ function edit_whitelist {
     new_entry="${new_entry,,}"
 
     if [[ "$new_entry" == -* ]]; then
-        remove_entry "$list" "$whitelist_file"
+        new_entry=$(echo "$new_entry" | cut -c 2-)
+        if ! grep -xFq "$new_entry" "$2"; then
+            echo -e "\nEntry not found in $1: $new_entry"
+            continue
+        fi
+        echo -e "\nRemoved from $1: $new_entry"
+        sed -i "/^$new_entry$/d" "$2"
         continue
     fi
 
@@ -144,33 +129,99 @@ function edit_whitelist {
         continue
     fi
 
-    add_entry "$list" "$whitelist_file"
+    echo -e "\nAdded to $1: $new_entry"
+    echo "$new_entry" >> "$2"
+
+    awk NF "$2" > tmp1.txt
+
+    sort tmp1.txt -o "$2"
+
+    rm tmp*.txt
 }
 
 function edit_blacklist {
     echo "Blacklist"
-    list="blacklist"
 
     read -p $'Enter the new entry (add \'-\' to remove entry):\n' new_entry
+            
+    remove_entry=0
+
+    if [[ "$new_entry" == -* ]]; then
+        new_entry=$(echo "$new_entry" | cut -c 2-)
+        remove_entry=1
+    fi
 
     new_entry="${new_entry,,}"
 
-    if [[ "$new_entry" == -* ]]; then
-        remove_entry "$list" "$blacklist_file"
+    if [[ "$new_entry" == www.* ]]; then
+        www_subdomain="${new_entry}"
+        new_entry=$(echo "$new_entry" | awk '{sub(/^www\./, "")}1')
+    else
+        www_subdomain="www.${new_entry}"
+    fi
+
+    echo "$new_entry" > tmp_entries.txt
+
+    echo "$www_subdomain" >> tmp_entries.txt
+            
+    sort tmp_entries.txt -o tmp_entries.txt
+            
+    if [[ "$remove_entry" -eq 1 ]]; then
+        if ! grep -xFqf tmp_entries.txt "$blacklist_file"; then
+            echo -e "\nDomain not found in blacklist: $new_entry"
+            continue
+        fi
+
+        echo -e "\nDomains removed:"
+        comm -12 "$blacklist_file" tmp_entries.txt
+
+        comm -23 "$blacklist_file" tmp_entries.txt > tmp1.txt
+
+        mv tmp1.txt "$blacklist_file"
+
+        rm tmp*.txt
+
         continue
     fi
 
     if ! [[ "$new_entry" =~ ^[[:alnum:].-]+\.[[:alnum:]]{2,}$ ]]; then
-        echo -e "\nInvalid entry. Not added."
+        echo -e "\nInvalid domain. Not added."
         continue
     fi
 
-    if grep -xFq "$new_entry" "$blacklist_file"; then
-        echo -e "\nThe domain is already in the blacklist. Not added."
-        continue
-    fi
+    touch tmp_alive_entries.txt
 
-    add_entry "$list" "$blacklist_file"
+    while read -r entry; do
+        if ! dig @1.1.1.1 "$entry" | grep -Fq 'NXDOMAIN'; then
+            echo "$entry" >> tmp_alive_entries.txt
+            fi
+        done < tmp_entries.txt
+
+        if ! [[ -s tmp_alive_entries.txt ]]; then
+            echo -e "\nThe domain is dead. Not added."
+            continue
+        fi
+
+        mv tmp_alive_entries.txt tmp_entries.txt
+  
+        # This checks if there are no unique entries in the new entries file
+        if [[ $(comm -23 tmp_entries.txt "$blacklist_file" | wc -l) -eq 0 ]]; then
+            echo -e "\nThe domain is already in the blacklist. Not added."
+            continue
+        fi
+
+        echo -e "\nDomains added:"
+        comm -23 tmp_entries.txt "$blacklist_file"
+
+        cat tmp_entries.txt >> "$blacklist_file" 
+
+        sort -u "$blacklist_file" -o "$blacklist_file"
+
+        awk NF "$blacklist_file" > tmp1.txt
+
+        sort tmp1.txt -o "$blacklist_file"
+
+        rm tmp*.txt
 }
 
 while true; do
