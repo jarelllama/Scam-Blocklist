ecb4c428690164cf44800319bc0f4b2ef76530a7
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-23 18:15:51 +0800

    Show failure when domain is found in updated toplist

--- workflow_scripts/toplist.sh
+++ workflow_scripts/toplist.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
+domains_file="domains"
+blacklist_file="blacklist.txt"
 toplist_file="toplist.txt"
-email="91372088+jarelllama@users.noreply.github.com"
-name="jarelllama"
+github_email="91372088+jarelllama@users.noreply.github.com"
+github_name="jarelllama"
 
 wget https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt -O "$toplist_file"
 
-git config user.email "$email"
-git config user.name "$name"
+git config user.email "$github_email"
+git config user.name "$github_name"
 
 git add "$toplist_file"
 git commit -m "Update $toplist_file"
 git push
+
+grep -xFf "$domains" "$toplist_file" | grep -vxFf "$blacklist_file" > tmp1.txt
+
+# The following code produces an error when a domain is found in the updated toplist. This allows the user to be informed via email
+
+if ! [[ -s tmp1.txt ]]; then
+    rm tmp*.txt
+    exit 0
+fi
+
+echo -e "\nDomains in toplist:"
+cat tmp1.txt
+
+rm tmp*.txt
+
+exit 1
