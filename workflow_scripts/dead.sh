da72cc17df1f7e3acb39d725d57c2974c86c9554
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-23 23:31:27 +0800

    Update dead.sh

--- workflow_scripts/dead.sh
+++ workflow_scripts/dead.sh
@@ -27,7 +27,7 @@ comm -23 tmp1.txt <(sort tmp_dead.txt) > "$domains_file"
 echo -e "\nDead domains:"
 cat tmp_dead.txt
 
-echo -e "\nTotal domains removed: $(wc -l < tmp_dead.txt)"
+#echo -e "\nTotal domains removed: $(wc -l < tmp_dead.txt)"
 
 rm tmp*.txt
 
