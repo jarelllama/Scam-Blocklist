01a47248ae32449df734ae51937e0cecb59c3921
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-23 15:54:14 +0800

    Update toplist.sh

--- workflow_scripts/toplist.sh
+++ workflow_scripts/toplist.sh
@@ -6,6 +6,9 @@ name="jarelllama"
 
 wget https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt -O "$toplist_file"
 
+git config user.email "$email"
+git config user.name "$name"
+
 git add "$toplist_file"
 git commit -m "Update $toplist_file"
 git push
