64e7d278e1b9af3253d904246389079bed70a7fa
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-24 23:40:30 +0800

    Delete test.sh

--- workflow_scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-github_email="91372088+jarelllama@users.noreply.github.com"
-github_name="jarelllama"
-git config user.email "$github_email"
-git config user.name "$github_name"
-git add temp.txt
-git commit -qm "Test commit"
-git push -q
