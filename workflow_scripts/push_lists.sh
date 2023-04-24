cbe54ad1da92c2910105fe38abec547e25844fe8
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-25 01:00:06 +0800

    Create push_lists.sh

---
+++ workflow_scripts/push_lists.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+raw_file="raw.txt"
+domains_file"domains.txt"
+adblock_file="adblock.txt"
+github_email="91372088+jarelllama@users.noreply.github.com"
+github_name="jarelllama"
+
+git config user.email "$github_email"
+git config user.name "$github_name"
+
+git add "$domains_file" "$adblock_file" "$raw_file"
+git commit -m "Update lists"
+git push
