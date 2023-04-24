729cab6d89120ccda3be4adfd508c22255a86991
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-24 23:12:23 +0800

    Create test.sh

---
+++ workflow_scripts/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+github_email="91372088+jarelllama@users.noreply.github.com"
+github_name="jarelllama"
+git config user.email "$github_email"
+git config user.name "$github_name"
+git add test.txt
+git commit -qm "Test commit"
+git push -q
