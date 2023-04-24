2436c840f619103b664da50d3c8475b9796158d7
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-25 00:57:44 +0800

    Remove push

--- workflow_scripts/add_raw_header.sh
+++ workflow_scripts/add_raw_header.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 raw_file="raw.txt"
-github_email="91372088+jarelllama@users.noreply.github.com"
-github_name="jarelllama"
 
 grep -vE '^(#|$)' "$raw_file" > tmp1.txt
 
@@ -11,10 +9,3 @@ echo "# Raw file for list generation
 " | cat - tmp1.txt > "$raw_file"
 
 rm tmp*.txt
-
-git config user.email "$github_email"
-git config user.name "$github_name"
-
-git add "$raw_file"
-git commit -m "Add header to $raw_file"
-git push
