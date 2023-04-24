58881e728a1e8c70358a5292e4e9e7d37659bd81
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-24 23:16:51 +0800

    Update test.sh

--- workflow_scripts/test.sh
+++ workflow_scripts/test.sh
@@ -3,6 +3,6 @@ github_email="91372088+jarelllama@users.noreply.github.com"
 github_name="jarelllama"
 git config user.email "$github_email"
 git config user.name "$github_name"
-git -f add test.txt
+git add temp.txt
 git commit -qm "Test commit"
 git push -q
