2f711a68c96f02282f5763fff6f577638add8ea2
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-25 21:10:24 +0800

    New method

--- workflow_scripts/update_count.sh
+++ workflow_scripts/update_count.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 readme="README.md"
+template="data/README.md.template"
 domains_file="domains.txt"
 adblock_file="adblock.txt"
-
-adblock_line_num=$(cat README.md | grep -n "| \[Adblock Plus\](https://raw" | cut -d ":" -f 1)
-
-domains_line_num=$(cat README.md | grep -n "| \[Domains\](https://raw" | cut -d ":" -f 1)
 
 adblock_count=$(grep -vE '^(!|$)' "$adblock_file" | wc -l)
 
 domains_count=$(grep -vE '^(#|$)' "$domains_file" | wc -l)
 
-sed -i "${adblock_line_num}s/[0-9]\{4,\}/$adblock_count/" "$readme"
+sed -i 's/adblock_count/'"$adblock_count"'/g' "$template"
 
-sed -i "${domains_line_num}s/[0-9]\{4,\}/$domains_count/" "$readme"
+sed -i 's/domains_count/'"$domains_count"'/g' "$template"
 
-rm *.tmp
+cp "$template" "$readme"
