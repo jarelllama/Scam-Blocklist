5c869d9635d98f4b4782e8343d5f5c656fceb919
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-25 09:12:46 +0800

    Optimise script

--- workflow_scripts/generate_domains.sh
+++ workflow_scripts/generate_domains.sh
@@ -7,13 +7,15 @@ grep -vE '^(#|$)' "$raw_file" > raw.tmp
 
 grep -vE '^(#|$)' "$domains_file" > domains.tmp
 
-if diff -q domains.tmp raw.tmp >/dev/null ; then
+if diff -q domains.tmp raw.tmp >/dev/null; then
    echo -e "\nNo changes. Exiting...\n"
    rm *.tmp
    exit 0
 fi
 
-diff -u domains.tmp raw.tmp | patch domains.tmp
+cp raw.tmp domains.tmp
+
+num_domains=$(wc -l < domains.tmp)
 
 echo "# Title: Jarelllama's Scam Blocklist
 # Description: Blocklist for scam sites extracted from Google
@@ -21,7 +23,7 @@ echo "# Title: Jarelllama's Scam Blocklist
 # License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 # Last modified: $(date -u)
 # Syntax: Domains
-# Total number of domains: $(wc -l < domains.tmp)
+# Total number of domains: $num_domains
 " | cat - domains.tmp > "$domains_file"
 
 rm *.tmp
