89543713648bfc3340e7b8be4519aad154d3046a
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-25 20:06:09 +0800

    Update update_count.sh

--- workflow_scripts/update_count.sh
+++ workflow_scripts/update_count.sh
@@ -4,9 +4,9 @@ readme="README.md"
 domains_file="domains.txt"
 adblock_file="adblock.txt"
 
-adblock_line_num=$(cat README.md | grep -n '| \[Adblock Plus\](https://raw' | cut -d ":" -f 1)
+adblock_line_num=$(cat README.md | grep -n "| \[Adblock Plus\](https://raw" | cut -d ":" -f 1)
 
-domains_line_num=$(cat README.md | grep -n '| \[Domains\](https://raw' | cut -d ":" -f 1)
+domains_line_num=$(cat README.md | grep -n "| \[Domains\](https://raw" | cut -d ":" -f 1)
 
 grep -vE '^(!|$)' "$adblock_file" > adblock.tmp
 
