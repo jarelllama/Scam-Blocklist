c3f05c2184737f0f439340c4d83e994beee1c367
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-25 16:27:30 +0800

    Update whitelist.sh

--- workflow_scripts/whitelist.sh
+++ workflow_scripts/whitelist.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-domains_file="domains"
+raw_file="data/raw.txt"
 whitelist_file="whitelist.txt"
 blacklist_file="blacklist.txt"
 
-grep -vE '^(#|$)' "$domains_file" > tmp1.txt
+grep -vE '^(#|$)' "$raw_file" > raw.tmp
 
-grep -Ff "$whitelist_file" tmp1.txt | grep -vxFf "$blacklist_file" > tmp_whitelisted.txt
+grep -Ff "$whitelist_file" raw.tmp | grep -vxFf "$blacklist_file" > whitelisted.tmp
 
-if ! [[ -s tmp_whitelisted.txt ]]; then
+if ! [[ -s whitelisted.tmp ]]; then
     echo -e "\nNo whitelisted domains found.\n"
-    rm tmp*.txt
+    rm *.tmp
     exit 0
 fi
 
 echo -e "\nWhitelisted domains:"
-cat tmp_whitelisted.txt
+cat whitelisted.tmp
 echo ""
 
-rm tmp*.txt
+rm *.tmp
 
 exit 1
