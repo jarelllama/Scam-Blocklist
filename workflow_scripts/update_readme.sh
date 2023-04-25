d8879c89f5dc667cc161b7444628dd3e7afd6ccf
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-26 01:13:33 +0800

    Update update_readme.sh

--- workflow_scripts/update_readme.sh
+++ workflow_scripts/update_readme.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-template="data/README.md.template"
 readme="README.md"
+template="data/README.md.template"
+
+adblock_count=$(grep -vE '^(!|$)' "$adblock_file" | wc -l)
+
+domains_count=$(grep -vE '^(#|$)' "$domains_file" | wc -l)
+
+sed -i 's/adblock_count/'"$adblock_count"'/g' "$template"
+
+sed -i 's/domains_count/'"$domains_count"'/g' "$template"
+
+sed -i 's/update_time/'"$(date -u +"%a %b %d %H:%m UTC")"'/g' "$template"
 
 cp "$template" "$readme"
 
