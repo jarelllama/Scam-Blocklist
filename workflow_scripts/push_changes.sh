d8fe56b4466299a9f39ad156534e949537a4b7ca
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-25 23:49:23 +0800

    Remove raw.txt

--- workflow_scripts/push_changes.sh
+++ workflow_scripts/push_changes.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 readme="README.md"
-raw_file="data/raw.txt"
 domains_file="domains.txt"
 adblock_file="adblock.txt"
 github_email="91372088+jarelllama@users.noreply.github.com"
@@ -10,6 +9,6 @@ github_name="jarelllama"
 git config user.email "$github_email"
 git config user.name "$github_name"
 
-git add "$domains_file" "$adblock_file" "$raw_file" "$readme"
+git add "$domains_file" "$adblock_file" "$readme"
 git commit -m "Update blocklist"
 git push
