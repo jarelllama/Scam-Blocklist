dae3f731c77d48517bffbfe706c852afc4cd22b8
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-25 20:01:45 +0800

    Fix

--- workflow_scripts/update_count.sh
+++ workflow_scripts/update_count.sh
@@ -4,28 +4,18 @@ readme="README.md"
 domains_file="domains.txt"
 adblock_file="adblock.txt"
 
-line_num=$(cat "$readme" | grep -xn "| Syntax | Domains/Entries |" | cut -d ":" -f 1)
+adblock_line_num=$(cat README.md | grep -n '| \[Adblock Plus\](https://raw' | cut -d ":" -f 1)
 
-adblock_line=$((line_num + 2))
-
-domains_line=$((line_num + 3))
-
-sed -i "${adblock_line}s/.*/ /" "$readme"
-
-sed -i "${domains_line}s/.*/ /" "$readme"
+domains_line_num=$(cat README.md | grep -n '| \[Domains\](https://raw' | cut -d ":" -f 1)
 
 grep -vE '^(!|$)' "$adblock_file" > adblock.tmp
 
 grep -vE '^(#|$)' "$domains_file" > domains.tmp
 
-adblock_num=$(wc -l < adblock.tmp)
+adblock_count=$(wc -l < adblock.tmp)
 
-domains_num=$(wc -l < domains.tmp)
+domains_count=$(wc -l < domains.tmp)
 
-new_adblock_line="| [Domains](https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/adblock.txt) | $adblock_num |"
+sed -i "${adblock_line_num}s/[1-9]\{3,\}/$adblock_count/" "$readme"
 
-new_domains_line="| [Domains](https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/domains.txt) | $domains_num |"
-
-awk -v line="$adblock_line" -v text="$new_adblock_line" 'NR==line {$0=text} {print}' "$readme" > "$readme"
-
-awk -v line="$domains_line" -v text="$new_domains_line" 'NR==line {$0=text} {print}' "$readme" > "$readme"
+sed -i "${domains_line_num}s/[1-9]\{3,\}/$domains_count/" "$readme"
