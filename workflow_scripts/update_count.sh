59c3d8f9f8cc8d01300d9bb5a7fb7d4d7c784313
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-26 13:52:22 +0800

    Add top scam TLDs stat

--- workflow_scripts/update_count.sh
+++ workflow_scripts/update_count.sh
@@ -20,6 +20,8 @@ fi
 
 sed -i 's/update_time/'"$(date -u +"%a %b %d %H:%M UTC")"'/g' "$template"
 
-cp "$template" "$readme"
+top_tlds=$(awk -F '.' '{print $NF}' data/raw.txt | sort | uniq -c | sort -nr | head -15 | awk '{print "| " $2, " | "$1 " |"}')
+
+awk -v var="$top_tlds" '{gsub(/top_tlds/,var)}1' "$template" > "readme"
 
 # Note that only the README file should be pushed
