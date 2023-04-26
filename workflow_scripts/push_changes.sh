198a23a5a8ffbf0e66dc1257857418ffbf64d2d4
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-26 23:10:26 +0800

    Merge update count

--- workflow_scripts/push_changes.sh
+++ workflow_scripts/push_changes.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
 readme="README.md"
+template="data/README.md"
 domains_file="domains.txt"
 adblock_file="adblock.txt"
 github_email="91372088+jarelllama@users.noreply.github.com"
 github_name="jarelllama"
+
+adblock_count=$(grep -vE '^(!|$)' "$adblock_file" | wc -l)
+
+domains_count=$(grep -vE '^(#|$)' "$domains_file" | wc -l)
+
+sed -i 's/adblock_count/'"$adblock_count"'/g' "$template"
+
+sed -i 's/domains_count/'"$domains_count"'/g' "$template"
+
+top_tlds=$(awk -F '.' '{print $NF}' "$raw_file" | sort | uniq -c | sort -nr | head -10 | awk '{print "| " $2, " | "$1 " |"}')
+
+awk -v var="$top_tlds" '{gsub(/top_tlds/,var)}1' "$template" > template.tmp
+
+if ! diff -q "$readme" template.tmp >/dev/null; then
+    sed -i 's/update_time/'"$(date -u +"%a %b %d %H:%M UTC")"'/g' template.tmp
+fi
+
+cp template.tmp "$readme"
 
 git config user.email "$github_email"
 git config user.name "$github_name"
@@ -13,6 +32,6 @@ git add "$domains_file" "$adblock_file"
 git commit -m "Build lists"
 
 git add "$readme"
-git commit -m "Update domains/entries count"
+git commit -m "Update README count"
 
 git push
