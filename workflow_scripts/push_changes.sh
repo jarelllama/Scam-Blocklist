bcbbf699eb542eb26281d90dd8686e168d1b0474
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-26 11:57:05 +0800

    Add readme as separate commit

--- workflow_scripts/push_changes.sh
+++ workflow_scripts/push_changes.sh
@@ -9,6 +9,10 @@ github_name="jarelllama"
 git config user.email "$github_email"
 git config user.name "$github_name"
 
-git add "$domains_file" "$adblock_file" "$readme"
+git add "$domains_file" "$adblock_file"
 git commit -m "Update blocklist"
+
+git add "$readme"
+git commit -m "Update domains/entries count"
+
 git push
