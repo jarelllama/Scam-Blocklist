c5f12a38817b3df64a47140f035915e9f557542d
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-26 20:20:43 +0800

    Rename redundant file

--- workflow_scripts/compress_adblock.sh
+++ workflow_scripts/compress_adblock.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 adblock_file="adblock.txt"
-redundant_entries="data/redundant_entries.txt"
+redundant_rules="data/redundant_rules.txt"
 github_email="91372088+jarelllama@users.noreply.github.com"
 github_name="jarelllama"
-
-touch "$redundant_entries"
 
 grep -vE '^(!|$)' "$adblock_file" > adblock.tmp
 
 while read -r entry; do
-    grep "\.${entry#||}$" adblock.tmp >> "$redundant_entries"
+    grep "\.${entry#||}$" adblock.tmp >> "$redundant_rules"
 done < adblock.tmp
 
 # The output has a high chance of having duplicates
-sort -u "$redundant_entries" -o "$redundant_entries"
+sort -u "$redundant_rules" -o "$redundant_rules"
 
 rm *.tmp
 
-if ! [[ -s "$redundant_entries" ]]; then
+if ! [[ -s "$redundant_rules" ]]; then
     echo -e "\nNo redundant rules found.\n"
     exit 0
 fi
@@ -26,6 +24,6 @@ fi
 git config user.email "$github_email"
 git config user.name "$github_name"
 
-git add "$redundant_entries"
+git add "$redundant_rules"
 git commit -qm "Compress $adblock_file"
 git push -q
