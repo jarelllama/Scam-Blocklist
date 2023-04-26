0e9ccc51d8f4ae0db7181b84306de6d4c34f2c55
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-26 14:00:20 +0800

    fix variable

--- workflow_scripts/update_count.sh
+++ workflow_scripts/update_count.sh
@@ -22,6 +22,6 @@ sed -i 's/update_time/'"$(date -u +"%a %b %d %H:%M UTC")"'/g' "$template"
 
 top_tlds=$(awk -F '.' '{print $NF}' data/raw.txt | sort | uniq -c | sort -nr | head -15 | awk '{print "| " $2, " | "$1 " |"}')
 
-awk -v var="$top_tlds" '{gsub(/top_tlds/,var)}1' "$template" > "readme"
+awk -v var="$top_tlds" '{gsub(/top_tlds/,var)}1' "$template" > "$readme"
 
 # Note that only the README file should be pushed
