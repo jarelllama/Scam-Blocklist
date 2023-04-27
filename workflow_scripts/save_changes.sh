64ebb7cb1469f029a5b5a0494c727013b6f94d50
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-27 13:35:04 +0800

    Update save_changes.sh

--- workflow_scripts/save_changes.sh
+++ workflow_scripts/save_changes.sh
@@ -2,6 +2,7 @@
 
 readme="README.md"
 template="data/README.md"
+count_history="data/count_history.txt"
 raw_file="data/raw.txt"
 domains_file="domains.txt"
 adblock_file="adblock.txt"
@@ -15,6 +16,24 @@ domains_count=$(grep -vE '^(#|$)' "$domains_file" | wc -l)
 sed -i 's/adblock_count/'"$adblock_count"'/g' "$template"
 
 sed -i 's/domains_count/'"$domains_count"'/g' "$template"
+
+todays_date=$(date -u +"%m%d%y")
+
+date_in_file=$(head -n 1 "$count_history")
+
+if [[ "$todays_date" != "$date_in_file" ]]; then
+    count_before=$(tail -n 1 "$count_history")
+
+    count_diff=$((adblock_count - count_before))
+
+    sed -i 's/found_yest/'"$diff"'/g' "$template"
+
+    > "$count_history"
+
+    echo "$todays_date" >> "$count_history"
+
+    echo "$adblock_count" >> "$count_history"
+fi
 
 top_tlds=$(awk -F '.' '{print $NF}' "$raw_file" | sort | uniq -c | sort -nr | head -10 | awk '{print "| " $2, " | "$1 " |"}')
 
