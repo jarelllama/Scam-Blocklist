b3f6097cfedf91a2ef15013fa790a48ba74f228b
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-27 09:45:00 +0800

    Optimizations

--- workflow_scripts/compress_adblock.sh
+++ workflow_scripts/compress_adblock.sh
@@ -8,18 +8,23 @@ github_name="jarelllama"
 grep -vE '^(!|$)' "$adblock_file" > adblock.tmp
 
 while read -r entry; do
-    grep "\.${entry#||}$" adblock.tmp >> "$redundant_rules"
+    grep "\.${entry#||}$" adblock.tmp >> redundant_rules.tmp
 done < adblock.tmp
 
-# The output has a high chance of having duplicates
-sort -u "$redundant_rules" -o "$redundant_rules"
-
-rm *.tmp
-
-if ! [[ -s "$redundant_rules" ]]; then
+if ! [[ -f redundant_rules.tmp ]]; then
     echo -e "\nNo redundant rules found.\n"
+    rm *.tmp
     exit 0
 fi
+
+# The output has a high chance of having duplicates
+sort -u redundant_rules.tmp -o "$redundant_rules"
+
+echo -e "\Redundant rules found:"
+cat redundant_rules.tmp
+echo ""
+
+rm *.tmp
 
 git config user.email "$github_email"
 git config user.name "$github_name"
