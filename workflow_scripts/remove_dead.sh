d0accc46237d7a07fa70c5d31373ebf1112ea652
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-28 10:24:35 +0800

    Add check for resurrected domains

--- workflow_scripts/remove_dead.sh
+++ workflow_scripts/remove_dead.sh
@@ -7,46 +7,79 @@ dead_domains_file="data/dead_domains.txt"
 github_email="91372088+jarelllama@users.noreply.github.com"
 github_name="jarelllama"
 
+git config user.email "$github_email"
+git config user.name "$github_name"
+
+function remove_dead {
+    grep -vxFf dead.tmp "$raw_file" > raw.tmp
+
+    mv raw.tmp "$raw_file"
+
+    grep -vxFf dead.tmp "$blacklist_file" > blacklist.tmp
+
+    mv blacklist.tmp "$blacklist_file"
+
+    awk '{print "||" $0 "^"}' dead.tmp > adblock_dead.tmp
+
+    grep -vxFf adblock_dead.tmp "$redundant_rules" > redundant_rules.tmp
+
+    mv redundant_rules.tmp "$redundant_rules"
+
+    cat dead.tmp >> "$dead_domains_file"
+
+    sort -u "$dead_domains_file" -o "$dead_domains_file"
+
+    echo -e "\nDead domains:"
+cat dead.tmp
+
+    echo -e "\nTotal domains removed: $(wc -l < dead.tmp)"
+    
+    git add "$raw_file" "$blacklist_file" "$redundant_rules" "$dead_domains_file"
+    git commit -qm "Remove dead domains"
+}
+
+function add_resurrected {
+    grep -vxFf dead_now_alive.tmp "$dead_domains_file" > "$dead_domains_file.tmp"
+
+    mv "$dead_domains_file.tmp" "$dead_domains_file"
+
+    cat dead_now_alive.tmp >> "$raw_file" 
+    
+    sort "$raw_file" -o "$raw_file"
+
+    echo -e "\nPreviously dead domains that are alive again:"
+cat dead_now_alive.tmp
+
+    echo -e "\nTotal domains added: $(wc -l < dead_now_alive.tmp)"
+    
+    git add "$raw_file" "$dead_domains_file"
+    git commit -qm "Add resurrected dead domains"
+}
+
 cat "$raw_file" | xargs -I{} -P8 bash -c "
   if dig @1.1.1.1 {} | grep -Fq 'NXDOMAIN'; then
       echo {} >> dead.tmp
   fi
 "
 
-if ! [[ -s dead.tmp ]]; then
-    echo -e "\nNo dead domains found.\n"
-    rm *.tmp
-    exit 0
+cat "$dead_domains_file" | xargs -I{} -P8 bash -c "
+  if ! dig @1.1.1.1 {} | grep -Fq 'NXDOMAIN'; then
+      echo {} >> dead_now_alive.tmp
+  fi
+"
+
+if [[ -s dead.tmp ]]; then
+    remove_dead
+else
+    echo -e "\nNo dead domains found."
 fi
 
-grep -vxFf dead.tmp "$raw_file" > raw.tmp
-
-mv raw.tmp "$raw_file"
-
-grep -vxFf dead.tmp "$blacklist_file" > blacklist.tmp
-
-mv blacklist.tmp "$blacklist_file"
-
-awk '{print "||" $0 "^"}' dead.tmp > adblock_dead.tmp
-
-grep -vxFf adblock_dead.tmp "$redundant_rules" > redundant_rules.tmp
-
-mv redundant_rules.tmp "$redundant_rules"
-
-cat dead.tmp >> "$dead_domains_file"
-
-sort -u "$dead_domains_file" -o "$dead_domains_file"
-
-echo -e "\nDead domains:"
-cat dead.tmp
-
-echo -e "\nTotal domains removed: $(wc -l < dead.tmp)\n"
+if [[ -s dead_now_alive.tmp ]]; then
+    add_resurrected
+fi
 
 rm *.tmp
 
-git config user.email "$github_email"
-git config user.name "$github_name"
+echo ""
 
-git add "$raw_file" "$blacklist_file" "$redundant_rules"
-git commit -qm "Remove dead domains"
 git push -q
