59b71346fbd5cbac2c862135e067c8fe746d843c
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-29 15:39:06 +0800

    Update and rename legacy/compress_adblock.sh to workflow_scripts/compress_adblock.sh

--- workflow_scripts/compress_adblock.sh
+++ workflow_scripts/compress_adblock.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 adblock_file="adblock.txt"
-redundant_rules="data/redundant_rules.txt"
+compressed_entries="data/compressed_entries.txt"
 github_email="91372088+jarelllama@users.noreply.github.com"
 github_name="jarelllama"
 
 grep -vE '^(!|$)' "$adblock_file" > adblock.tmp
 
-echo -e "\nRedundant rules (if any):"
+echo -e "\nRedundant entries (if any):"
 
 # I've tried using xarg parallelization here to no success
 while read -r entry; do
     grep "\.${entry#||}$" adblock.tmp | awk -v entry="$entry" '{print $0 " made redundant by " entry}'  
-    grep "\.${entry#||}$" adblock.tmp >> redundant_rules.tmp
+    grep "\.${entry#||}$" adblock.tmp >> compressed_entries.tmp
 done < adblock.tmp
 
-if ! [[ -s redundant_rules.tmp ]]; then
+if ! [[ -s compressed_entries.tmp ]]; then
     echo -e "\nNo redundant rules found.\n"
     rm *.tmp
     exit 0
 fi
 
-cat redundant_rules.tmp >> "$redundant_rules"
+cat compressed_entries.tmp >> "$compressed_entries"
 
 # The output has a high chance of having duplicates
-sort -u "$redundant_rules" -o "$redundant_rules"
+sort -u "$compressed_entries" -o "$compressed_entries"
 
 rm *.tmp
 
@@ -33,6 +33,6 @@ echo ""
 git config user.email "$github_email"
 git config user.name "$github_name"
 
-git add "$redundant_rules"
-git commit -qm "Compress Adblock rules"
+git add "$compressed_entries"
+git commit -qm "Compress Adblock entries"
 git push -q
