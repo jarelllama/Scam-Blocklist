ea521b8e927a79530055a6f534d2b5de0d5f6f2c
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-29 13:39:52 +0800

    Use subdomains file

--- workflow_scripts/add_subdomains.sh
+++ workflow_scripts/add_subdomains.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 raw_file="data/raw.txt"
+subdomains_file="data/subdomains.txt"
 github_email="91372088+jarelllama@users.noreply.github.com"
 github_name="jarelllama"
 
@@ -14,52 +15,48 @@ awk '{print "www."$0}' no_www.tmp > with_www_new.tmp
 
 cat no_www_new.tmp with_www_new.tmp > flipped.tmp
 
-touch flipped_dead.tmp
+touch flipped_alive.tmp
 
 cat flipped.tmp | xargs -I{} -P8 bash -c "
-    if dig @1.1.1.1 {} | grep -Fq 'NXDOMAIN'; then
-        echo {} >> flipped_dead.tmp
+    if ! dig @1.1.1.1 {} | grep -Fq 'NXDOMAIN'; then
+        echo {} >> flipped_alive.tmp
     fi
 "
-
-grep -vxFf flipped_dead.tmp flipped.tmp > flipped_alive.tmp
 
 cat flipped_alive.tmp "$raw_file" > raw.tmp
 
 grep -v '^www\.' raw.tmp > no_www.tmp
 
-awk '{print "m."$0}' no_www.tmp > m_subdomain.tmp
+touch subdomain_alive.tmp
 
-touch m_subdomain_dead.tmp
+while read -r subdomain; do
+    awk -v subdomain="$subdomain" '{print subdomain"."$0}' no_www.tmp > subdomain.tmp
 
-cat m_subdomain.tmp | xargs -I{} -P8 bash -c "
-    if dig @1.1.1.1 {} | grep -Fq 'NXDOMAIN'; then
-        echo {} >> m_subdomain_dead.tmp
-    fi
-"
+    cat subdomain.tmp | xargs -I{} -P4 bash -c "
+        if ! dig @1.1.1.1 {} | grep -Fq 'NXDOMAIN'; then
+            echo {} >> subdomain_alive.tmp
+        fi
+    "
 
-grep -vxFf m_subdomain_dead.tmp m_subdomain.tmp > m_subdomain_alive.tmp
+    cat subdomain_alive.tmp >> raw.tmp
+done < "$subdomains_file"
 
-cat m_subdomain_alive.tmp flipped_alive.tmp > new_entries.tmp
+sort raw.tmp -o raw.tmp
 
-sort new_entries.tmp -o new_entries.tmp
+comm -23 raw.tmp "$raw_file" > unique.tmp
 
-comm -23 new_entries.tmp "$raw_file" > new_entries_unique.tmp
-
-if ! [[ -s new_entries_unique.tmp ]]; then
+if ! [[ -s unique.tmp ]]; then
     echo -e "\nNo domains added.\n"
     rm *.tmp
     exit 0
 fi
 
-cat new_entries_unique.tmp >> "$raw_file"
-
-sort "$raw_file" -o "$raw_file"
+mv raw.tmp "$raw_file"
 
 echo -e "\nDomains added:"
-cat new_entries_unique.tmp
+cat unique.tmp
 
-echo -e "\nTotal domains added: $(wc -l < new_entries_unique.tmp)\n"
+echo -e "\nTotal domains added: $(wc -l < unique.tmp)\n"
 
 rm *.tmp
 
