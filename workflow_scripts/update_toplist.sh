ad563de186a7e2b0e771de257f1e04e700064176
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-30 21:13:42 +0800

    Cosmetic changes

--- workflow_scripts/update_toplist.sh
+++ workflow_scripts/update_toplist.sh
@@ -3,8 +3,8 @@
 raw_file="data/raw.txt"
 blacklist_file="blacklist.txt"
 toplist_file="data/toplist.txt"
-github_email="91372088+jarelllama@users.noreply.github.com"
-github_name="jarelllama"
+github_email='91372088+jarelllama@users.noreply.github.com'
+github_name='jarelllama'
 
 wget -q https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt -O "$toplist_file"
 
@@ -18,14 +18,14 @@ git push -q
 grep -xFf "$raw_file" "$toplist_file" | grep -vxFf "$blacklist_file" > in_toplist.tmp
 
 if ! [[ -s in_toplist.tmp ]]; then
-    echo -e "\nNo domains found in the updated toplist.\n"
+    echo -e "\nNo domains found in toplist.\n"
     rm *.tmp
     exit 0
 fi
 
 echo -e "\nDomains in toplist:"
 cat in_toplist.tmp
-echo ""
+echo
 
 rm *.tmp
 
