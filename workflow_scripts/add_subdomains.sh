9eb47e680d5d6e291c23304a46c519d713ed10c3
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-04-30 17:32:53 +0800

    Optimizations

--- workflow_scripts/add_subdomains.sh
+++ workflow_scripts/add_subdomains.sh
@@ -21,33 +21,29 @@ touch subdomains_alive.tmp
 while read -r subdomain; do
     awk -v subdomain="$subdomain" '{print subdomain"."$0}' base_domains.tmp > subdomains.tmp
 
-    comm -23 subdomains.tmp "$raw_file" > unique_subdomains.tmp
+    comm -23 subdomains.tmp "$raw_file" > new_subdomains.tmp
 
-    cat unique_subdomains.tmp | xargs -I{} -P8 bash -c "
+    cat new_subdomains.tmp | xargs -I{} -P8 bash -c "
         if ! dig @1.1.1.1 {} | grep -Fq 'NXDOMAIN'; then
             echo {} >> subdomains_alive.tmp
         fi
     "
 done < "$subdomains_file"
 
-cat subdomains_alive.tmp "$raw_file" > raw.tmp
-
-sort -u raw.tmp -o raw.tmp
-
-comm -23 raw.tmp "$raw_file" > new_entries.tmp
-
-if ! [[ -s new_entries.tmp ]]; then
+if ! [[ -s subdomains_alive.tmp ]]; then
     echo -e "\nNo domains added.\n"
     rm *.tmp
     exit 0
 fi
 
-cp raw.tmp "$raw_file"
+cat subdomains_alive.tmp >> "$raw_file"
+
+sort "$raw_file" -o "$raw_file"
 
 echo -e "\nDomains added:"
-cat new_entries.tmp
+cat subdomains_alive.tmp
 
-echo -e "\nTotal domains added: $(wc -l < unique.tmp)\n"
+echo -e "\nTotal domains added: $(wc -l < subdomains_alive.tmp)\n"
 
 rm *.tmp
 
