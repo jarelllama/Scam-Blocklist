6ca9bce436b18b2f1ed919ac34e986e923de9698
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-05-01 23:51:43 +0800

    Download subdomains toplist

--- workflow_scripts/update_toplists.sh
+++ workflow_scripts/update_toplists.sh
@@ -3,6 +3,7 @@
 raw_file="data/raw.txt"
 blacklist_file="blacklist.txt"
 toplist_file="data/toplist.txt"
+subdomains_toplist_file="data/subdomains_toplist.txt"
 github_email='91372088+jarelllama@users.noreply.github.com'
 github_name='jarelllama'
 
@@ -10,9 +11,10 @@ git config user.email "$github_email"
 git config user.name "$github_name"
 
 wget -q https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist.txt -O "$toplist_file"
+wget -q https://raw.githubusercontent.com/hagezi/dns-data-collection/main/top/toplist-merged.txt -O "$subdomains_toplist_file"
 
 git add "$toplist_file"
-git commit -qm "Update toplist.txt"
+git commit -qm "Update toplists"
 git push -q
 
 grep -xFf "$raw_file" "$toplist_file" | grep -vxFf "$blacklist_file" > in_toplist.tmp
