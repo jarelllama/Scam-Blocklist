59909bd945d3fe389abb41c2924a8f65faca720b
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-05-02 21:18:14 +0800

    Cosmetic script change

--- retrieve.sh
+++ retrieve.sh
@@ -46,9 +46,9 @@ fi
 echo -e "\nSearch filter: $time_filter"
 echo "Search terms:"
 
-user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-
 declare -A retrieved_domains
+
+user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 
 # A blank IFS ensures the entire search term is read
 while IFS= read -r term; do
