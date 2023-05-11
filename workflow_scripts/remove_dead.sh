2f95500bb93067b8cb807f55f955e645153383b8
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-05-11 18:14:39 +0800

    Show all logs before added/removed domains count

--- workflow_scripts/remove_dead.sh
+++ workflow_scripts/remove_dead.sh
@@ -5,28 +5,6 @@ blacklist_file="blacklist.txt"
 compressed_entries_file="data/compressed_entries.txt"
 dead_domains_file="data/dead_domains.txt"
 
-function check_resolving() {
-    > dead.tmp
-    
-    echo -e "\nLog:"
-
-    cat "$1" | xargs -I{} -P8 bash -c '
-        domain="$1"
-        while true; do
-            dig=$(dig @1.1.1.1 "$domain")
-            [[ "$dig" =~ error|timed\ out ]] || break
-            echo "$domain timed out. Retrying..."
-            sleep 1
-        done
-        if [[ "$dig" == *"NXDOMAIN"* ]]; then
-            echo "$domain (dead)"
-            echo "$domain" >> dead.tmp
-        fi
-    ' -- {}
-    
-    sort -u dead.tmp -o dead.tmp
-}
-
 function remove_dead {
     comm -23 "$raw_file" dead.tmp > raw.tmp
     mv raw.tmp "$raw_file"
@@ -51,7 +29,8 @@ function remove_dead {
 }
 
 function add_resurrected {
-    mv dead.tmp "$dead_domains_file"
+    comm -23 "$dead_domains_file" alive.tmp > dead_domains.tmp
+    mv dead_domains.tmp "$dead_domains_file"
 
     cat alive.tmp >> "$raw_file" 
     sort "$raw_file" -o "$raw_file"
@@ -65,7 +44,45 @@ function add_resurrected {
     git commit -qm "Add resurrected domains"
 }
 
-check_resolving "$raw_file"
+> dead.tmp
+    
+echo -e "\nChecking for dead domains...\n"
+
+cat "$raw_file" | xargs -I{} -P8 bash -c '
+    domain="$1"
+    while true; do
+        dig=$(dig @1.1.1.1 "$domain")
+        [[ "$dig" =~ error|timed\ out ]] || break
+        echo "$domain timed out."
+        sleep 1
+    done
+    if [[ "$dig" == *"NXDOMAIN"* ]]; then
+        echo "$domain (dead)"
+        echo "$domain" >> dead.tmp
+    fi
+' -- {}
+    
+sort -u dead.tmp -o dead.tmp
+    
+> alive.tmp
+
+echo -e "\nChecking for resurrected domains...\n"
+
+cat "$dead_domains_file" | xargs -I{} -P8 bash -c '
+    domain="$1"
+    while true; do
+        dig=$(dig @1.1.1.1 "$domain")
+        [[ "$dig" =~ error|timed\ out ]] || break
+        echo "$domain timed out."
+        sleep 1
+    done
+    if ! [[ "$dig" == *"NXDOMAIN"* ]]; then
+        echo "$domain (alive)"
+        echo "$domain" >> alive.tmp
+    fi
+' -- {}
+    
+sort -u alive.tmp -o alive.tmp
 
 if [[ -s dead.tmp ]]; then
     remove_dead
@@ -73,12 +90,12 @@ else
     echo -e "\nNo dead domains found."
 fi
 
-check_resolving "$dead_domains_file"
-
-comm -23 "$dead_domains_file" dead.tmp > alive.tmp
-
 if [[ -s alive.tmp ]]; then
     add_resurrected
+else
+    echo -e "\nNo resurrected domains found."
 fi
+
+echo
 
 rm ./*.tmp
