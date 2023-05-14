90920b1b5fe59c4582eb54df772993e0cc703f2c
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-05-14 10:18:33 +0800

    update workflow_scripts/build_adblock.sh

--- workflow_scripts/build_adblock.sh
+++ workflow_scripts/build_adblock.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
 raw_file="data/raw.txt"
-list="adblock.txt"
+syntax="$1"
+path="$2"
+before="$3"
+after="$4"
+comment="$5"
 
 trap "find . -maxdepth 1 -type f -name '*.tmp' -delete" EXIT
 
-awk '{print "||" $0 "^"}' "$raw_file" > 1.tmp
-sort 1.tmp -o list.tmp
+awk -v before="$before" -v after="$after" '{print before $0 after}' "$raw_file" > "${path}.tmp"
 
-grep -vE '^!' "$list" > previous.tmp
+sort "${path}.tmp" -o "${path}.tmp"
 
-if diff -q previous.tmp list.tmp >/dev/null; then
+grep -vE "^${comment}" "$path" > previous.tmp
+
+if diff -q previous.tmp "${path}.tmp" >/dev/null; then
    echo -e "\nNo changes.\n"
    exit 0
 fi
 
 num_before=$(wc -l < previous.tmp)
 
-num_after=$(wc -l < list.tmp)
+num_after=$(wc -l < "${path}.tmp")
 
 echo -e "\nTotal entries before: $num_before"
 echo "Difference: $((num_after - num_before))"
 echo -e "Final entries after: $num_after\n"
 
-echo "! Title: Jarelllama's Scam Blocklist
-! Description: Blocklist for scam sites automatically retrieved from Google Search
-! Homepage: https://github.com/jarelllama/Scam-Blocklist
-! License: GNU GPLv3 (https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/LICENSE.md)
-! Version: $(date -u +"%m.%d.%H%M%S.%Y")
-! Last modified: $(date -u)
-! Expires: 4 hours
-! Syntax: Adblock Plus
-! Total number of entries: $num_after
-!" | cat - list.tmp > "$list"
+echo "${comment} Title: Jarelllama's Scam Blocklist
+${comment} Description: Blocklist for scam sites automatically retrieved from Google Search
+${comment} Homepage: https://github.com/jarelllama/Scam-Blocklist
+${comment} License: GNU GPLv3 (https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/LICENSE.md)
+${comment} Version: $(date -u +"%m.%d.%H%M%S.%Y")
+${comment} Last modified: $(date -u)
+${comment} Expires: 4 hours
+${comment} Syntax: $syntax
+${comment} Total number of entries: $num_after
+${comment}" | cat - "${path}.tmp" > "$path"
