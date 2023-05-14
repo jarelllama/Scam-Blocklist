9b238e45033f8ad56dffd44b1c707069635e4123
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-05-14 15:57:36 +0800

    update optimize.sh

--- optimize.sh
+++ optimize.sh
@@ -5,11 +5,11 @@ optimise_whitelist="data/optimise_whitelist"
 
 trap "find . -maxdepth 1 -type f -name '*.tmp' -delete" exit
 
-function print_domains() {
-    domains=$1
-    numbered_domains=$(echo "$domains" | awk '{print NR ". " $0}')
-    echo "$numbered_domains"
-}
+#function print_domains() {
+#    domains=$1
+#    numbered_domains=$(echo "$domains" | awk '{print NR " " $0}')
+#    echo "$numbered_domains"
+#}
 
 while true; do
     domains=$(grep -E '\..*\.' data/raw.txt \
@@ -19,26 +19,16 @@ while true; do
         | uniq -d \
         | grep -vF 'shop')
 
-    print_domains "$domains"
+    numbered_domains=$(echo "$output" | awk '{print NR " " $0}')
+    echo "$numbered_domains"
 
     read -rp "Select a domain ('x' to exit): " chosen_number
 
     [[ "$chosen_number" == 'x' ]] && exit 0
 
-    chosen_domain=$(echo "$domains" | awk -v n="$chosen_number" '$0 ~ n {print $0}')
+    chosen_domain=$(echo "$domains" | awk -v n="$chosen_number" '$1 == n {print $2}')
 
     echo "$chosen_domain"
 
     read -rp "Add \"$chosen_domain\" to the blacklist (b) or whitelist (w)? " blacklist_or_whitelist
-
-  # Add the domain to the specified list
-  if [[ "$blacklist_or_whitelist" == "b" ]]; then
-    blacklist+=("$chosen_domain")
-    echo "\"$chosen_domain\" added to the blacklist."
-  elif [[ "$blacklist_or_whitelist" == "w" ]]; then
-    whitelist+=("$chosen_domain")
-    echo "\"$chosen_domain\" added to the whitelist."
-  else
-    echo "Invalid option. \"$chosen_domain\" was not added to any list."
-  fi
 done
