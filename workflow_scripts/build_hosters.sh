9cc2f0b4cb09d66cc9628cc5fb62bde1c4b143b9
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-05-16 15:38:29 +0800

    update workflow_scripts/build_hosters.sh

--- workflow_scripts/build_hosters.sh
+++ workflow_scripts/build_hosters.sh
@@ -29,8 +29,8 @@ echo "Total entries before: $num_before"
 echo "Difference: $((num_after - num_before))"
 echo -e "Final entries after: $num_after\n"
 
-echo "${comment} Title: Jarelllama's Scam  Blocklist Malicious Hosters Edition
-${comment} Description: Blocklist for scam sites automatically retrieved from Google Search
+echo "${comment} Title: Jarelllama's Malicious Hosters Blocklist
+${comment} Description: Blocklist for malicious hosters automatically retrieved from Google Search
 ${comment} Homepage: https://github.com/jarelllama/Scam-Blocklist
 ${comment} License: GNU GPLv3 (https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/LICENSE.md)
 ${comment} Version: $(date -u +"%m.%d.%H%M%S.%Y")
