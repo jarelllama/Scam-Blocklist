9527694d577171d4859be1f584e4e120d5388d49
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-05-16 21:58:37 +0800

    Streamline script

--- workflow_scripts/build_scams.sh
+++ workflow_scripts/build_scams.sh
@@ -1,45 +1,95 @@
 #!/bin/bash
 
-raw_file="data/raw.txt"
-file='scams.txt'
-syntax="$1"
-comment='#'
-
 trap "find . -maxdepth 1 -type f -name '*.tmp' -delete" EXIT
 
-if [[ $(echo "$syntax" | grep -i "adblock") ]]; then
-    syntax='Adblock Plus'
-    path="lists/adblock/${file}"
-    comment='!'
-    before='||'
-    after='^'
-elif [[ $(echo "$syntax" | grep -i "wildcard domains") ]]; then
-    syntax='Wildcard Domains' 
-    path="lists/wildcard_domains/${file}"
-    before=''
-    after=''
-elif [[ $(echo "$syntax" | grep -i "wildcard asterisk") ]]; then
-    syntax='Wildcard Asterisk' 
-    path="lists/wildcard_asterisk/${file}"
-    before='*.'
-    after=''
-elif [[ $(echo "$syntax" | grep -i "dnsmasq") ]]; then
-    syntax='Dnsmasq' 
-    path="lists/dnsmasq/${file}"
-    before='address=/'
-    after='/#'
-elif [[ $(echo "$syntax" | grep -i "unbound") ]]; then
-    syntax='Unbound' 
-    path="lists/unbound/${file}"
-    before='local-zone: "'
-    after='." always_nxdomain'
+if [[ "$#" -eq 0 ]]; then
+    echo -e "No options provided"
+    exit 1
+fi
+
+title="Jarelllama's Scam Blocklist"
+description='Blocklist for scam sites automatically retrieved from Google Search'
+expires='4'
+comment='#'
+
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        -i | --input)
+            input="$2"
+            ;;
+        -o | --output)
+            output="$2"
+            ;;
+        -t | --title)
+            title="$2"
+            ;;
+        -d | --description)
+            description="$2"
+            ;;
+        -e | --expires)
+            expires="$2"
+            ;;
+        --adblock)
+            syntax='Adblock Plus'
+            path="lists/adblock/${output}"
+            comment='!'
+            before='||'
+            after='^'
+            ;;
+        --dnsmasq)   
+            syntax='Dnsmasq' 
+            path="lists/dnsmasq/${output}"
+            before='address=/'
+            after='/#'
+            ;;
+         --unbound)       
+            syntax='Unbound' 
+            path="lists/unbound/${output}"
+            before='local-zone: "'
+            after='." always_nxdomain'
+            ;;
+        --wc-asterisk)
+            syntax='Wildcard Asterisk' 
+            path="lists/wildcard_asterisk/${output}"
+            before='*.'
+            after=''
+            ;;
+        --wc-domains)
+            syntax='Wildcard Domains' 
+            path="lists/wildcard_domains/${output}"
+            before=''
+            after=''
+            ;;
+        *)
+            echo "Invalid option: --{$1}"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [[ -z "$input" ]]; then
+    echo "No input file provided"
+    exit 1
+fi
+
+if [[ -z "$output" ]]; then
+    echo "No output file provided"
+    exit 1
+fi
+
+if [[ -z "$syntax" ]]; then
+    echo "No syntax provided"
+    exit 1
 fi
 
 echo -e "\nBuilding ${syntax}..."
 
 touch "$path"
 
-awk -v before="$before" -v after="$after" '{print before $0 after}' "$raw_file" > "${path}.tmp"
+awk -v before="$before" -v after="$after" \
+    '{print before $0 after}' \
+    "$input" > "${path}.tmp"
 
 sort "${path}.tmp" -o "${path}.tmp"
 
@@ -54,23 +104,22 @@ if diff -q previous.tmp "${path}.tmp" >/dev/null; then
 fi
 
 num_before=$(wc -l < previous.tmp)
-
 num_after=$(wc -l < "${path}.tmp")
-
-echo "Total entries before: $num_before"
+echo "Total entries before: ${num_before}"
 echo "Difference: $((num_after - num_before))"
-echo -e "Final entries after: $num_after\n"
+echo "Final entries after: ${num_after}"
 
-echo "${comment} Title: Jarelllama's Scam Blocklist
-${comment} Description: Blocklist for scam sites automatically retrieved from Google Search
+echo "${comment} Title: ${title}
+${comment} Description: ${description}
 ${comment} Homepage: https://github.com/jarelllama/Scam-Blocklist
 ${comment} License: GNU GPLv3 (https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/LICENSE.md)
 ${comment} Version: $(date -u +"%m.%d.%H%M%S.%Y")
 ${comment} Last modified: $(date -u)
-${comment} Expires: 4 hours
-${comment} Syntax: $syntax
-${comment} Total number of entries: $num_after
+${comment} Expires: ${expires} hours
+${comment} Syntax: ${syntax}
+${comment} Total number of entries: ${num_after}
 ${comment}" | cat - "${path}.tmp" > "$path"
 
+echo
 git add "$path"
 git commit -qm "Build ${syntax}"
