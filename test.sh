4652abb7ae47dfe1243eeb3522d29c3a75e1f92e
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-03-17 11:52:59 +0800

    Write test script

---
+++ test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+raw_file='data/raw.txt'
+wildcards_file='data/wildcards.txt'
+blacklist_file='data/blacklist.txt'
+
+[[ "$CI" != true ]] && exit  # Do not allow running locally
+
+function main {
+    prepare_sample  # Prepare sample files
+
+    bash retrieve.sh  # Run retrieval script
+    # Check returned error code
+    if [[ 1 -eq 1 ]]; then
+        printf "Script returned an error\n"
+        exit 1
+    fi
+
+    # Check script output
+    if cmp -s "$raw_file" output.tmp; then
+        printf "Output is as expected.\n"
+        exit
+    fi
+    printf "Output is not as expected:\n"
+    cat "$raw_file"
+    printf "\n"
+    exit 1
+}
+
+function prepare_sample {
+    printf "in.blocklist.com" > "$raw_file"  # Prepare sample raw file
+    printf "wildcard.in.blocklist.com" >> "$raw_file"
+    printf "wildcard.in.blocklist.com" > "$wildcards_file"  # Prepare sample wildcards file
+    printf "blacklisted.forum.com" > "$blacklist_file"  # Prepare sample blacklist file
+
+    cat << EOF > input.tmp  # Prepare sample input data
+blacklisted.forum.com
+in.blocklist.com
+m.to.block.1.com
+match.wildcard.in.blocklist.com
+to.block.1.com
+to.block.3.com
+whitelisted.forum.com
+whitelisted.tld.gov
+wildcard.in.blocklist.com
+www.to.block.2.com
+EOF
+
+    cat << EOF > output.tmp  # Prepare expected result
+blacklisted.forum.com
+in.blocklist.com
+to.block.1.com
+to.block.2.com
+to.block.3.com
+wildcard.in.blocklist.com
+EOF
+
+    split -n l/3 input.tmp  # Split the 10 domains into 3 source files
+    mv xaa data/domains_aa419.tmp
+    mv xab data/domains_google_search_test_search_term_1.tmp
+    mv xac data/domains_google_search_test_search_term_2.tmp
+}
+
+main
