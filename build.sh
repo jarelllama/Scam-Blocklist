c4427590734f5a43bb51aa0bbb87f49f1ad0be8d
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-03-22 15:08:38 +0800

    Update readme

--- build.sh
+++ build.sh
@@ -20,6 +20,9 @@ function main {
 function update_readme {
     cat << EOF > README.md
 # Jarelllama's Scam Blocklist
+[![Retrieve domains](https://github.com/jarelllama/Scam-Blocklist/actions/workflows/retrieve.yml/badge.svg)](https://github.com/jarelllama/Scam-Blocklist/actions/workflows/retrieve.yml)
+[![Check lists](https://github.com/jarelllama/Scam-Blocklist/actions/workflows/check.yml/badge.svg)](https://github.com/jarelllama/Scam-Blocklist/actions/workflows/check.yml)
+[![Test functions](https://github.com/jarelllama/Scam-Blocklist/actions/workflows/test.yml/badge.svg)](https://github.com/jarelllama/Scam-Blocklist/actions/workflows/test.yml)
 
 Blocklist for scam sites retrieved from Google Search and public databases, automatically updated daily at 17:00 UTC.
 
