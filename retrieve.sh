b1c5a07422fa7deb193ad257080511c954ba71d2
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-03-24 15:19:34 +0800

    Update

--- retrieve.sh
+++ retrieve.sh
@@ -372,7 +372,7 @@ function log_source {
         -v toplist_count="$toplist_count" -v toplist_domains="$(printf "%s" "$domains_in_toplist" | tr '\n' ' ')" -v queries="$query_count" -v rate_limited="$rate_limited" -v time="$time_format" \
         'BEGIN {print time","source","search_term","raw","final","whitelist","dead","redundant","toplist_count","toplist_domains","queries","rate_limited",no"}' >> "$source_log"
     [[ "$source" == 'Google Search' ]] && source="\"${search_term:0:100}...\""
-    printf "Source: %s\nRaw: %s  Final: %s  Whitelisted: %s  Dead: %s  Toplist: %s\n" "$source" "$unfiltered_count" "$filtered_count" "$total_whitelisted_count" "$dead_count" "$toplist_count"
+    printf "Source: %s\nRaw:%4s  Final:%4s  Whitelisted:%4s  Dead:%4s  Toplist:%4s\n" "$source" "$unfiltered_count" "$filtered_count" "$total_whitelisted_count" "$dead_count" "$toplist_count"
     printf "%s\n" "------------------------------------------------------------"
 }
 
