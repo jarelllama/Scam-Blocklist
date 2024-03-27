e2c203dcad21bb81ae69ace7a038eb1e3b49a849
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-03-27 21:14:20 +0800

    Test

--- retrieve.sh
+++ retrieve.sh
@@ -169,7 +169,7 @@ function search_google {
         query_params="cx=${google_search_id}&key=${google_search_api_key}&exactTerms=${encoded_search_term}&start=${start}&excludeTerms=scam&filter=0"
         page_results=$(curl -s "${url}?${query_params}")
 
-        "$page_results" >> test.txt  # FOR DEBUGGING
+        printf "%s\n\n\n\n\n\n" "$page_results" >> test.txt  # FOR DEBUGGING
 
         # Use next API key if first key is rate limited
         if grep -qF 'rateLimitExceeded' <<< "$page_results"; then
