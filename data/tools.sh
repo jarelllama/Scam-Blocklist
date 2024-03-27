bf845e3e413be8daf608995a551f87787e265bc7
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-03-28 00:38:24 +0800

    Add parked domains check for light

--- data/tools.sh
+++ data/tools.sh
@@ -21,4 +21,37 @@ function format {
     esac
 }
 
+function remove_parked_domains {
+    raw_file="$1"
+    parked_terms_file='data/parked_terms.txt'
+    parked_domains_file='data/parked_domains.txt'
+
+    split -n 12 -d "$raw_file"
+    check_for_parked "x00" & check_for_parked "x01" &
+    check_for_parked "x02" & check_for_parked "x03" &
+    check_for_parked "x04" & check_for_parked "x05" &
+    check_for_parked "x06" & check_for_parked "x07" &
+    check_for_parked "x08" & check_for_parked "x09" &
+    check_for_parked "x10" & check_for_parked "x11"
+
+    find . -maxdepth 1 -type f -name "parked_domains_x*.tmp" -delete
+    find . -maxdepth 1 -type f -name "x*" -delete
+}
+
+function check_for_parked {
+    total=$(wc -l < "$1")
+    count=1
+    # Check for parked message in site's HTML
+    while read -r domain; do
+        (( "$count" % 10 == 0 )) && printf "%s/%s\n" "$count" "$total"
+        if grep -qiFf "$parked_terms_file" <<< "$(curl -sL --max-time 1 "http://${domain}/")"; then
+            printf "%s | Parked: %s\n" "$1" "$domain"
+            printf "%s\n" "$domain" >> "parked_domains_${1}.tmp"
+        fi
+        ((count++))
+    done < "$1"
+    [[ -f "parked_domains_${1}.tmp" ]] && cat "parked_domains_${1}.tmp" >> "$parked_domains_file"
+}
+
 [[ "$1" == 'format' ]] && format "$2"
+[[ "$1" == 'parked' ]] && remove_parked_domains "$2"
