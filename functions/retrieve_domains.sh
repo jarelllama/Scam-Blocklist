64b57ed639012808977c632c85a25221abbf0c61
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-03-30 15:57:32 +0800

    Test

--- functions/retrieve_domains.sh
+++ functions/retrieve_domains.sh
@@ -30,7 +30,7 @@ function main {
 
 function source {
     # Check for existing pending domains file
-    [[ -d data/pending ]] && { use_pending=true; printf "\nUsing existing lists of retrieved domains.\n" | colout . white faint; }
+    [[ -d data/pending ]] && { use_pending=true; printf "\nUsing existing lists of retrieved domains.\n" | colout . white faint.bold; }
     [[ -f data/pending/domains_manual.tmp ]] && source_manual  # Retrieve manually added domains
     mkdir -p data/pending
     source_aa419
@@ -153,7 +153,7 @@ function source_google_search {
     # Retrieve new domains
     while read -r search_term; do  # Loop through search terms
         # Return if rate limited
-        [[ "$rate_limited" == true ]] && { printf "\nBoth Google Search API keys are rate limited.\n" | colout . red faint; return; }
+        [[ "$rate_limited" == true ]] && { printf "\nBoth Google Search API keys are rate limited.\n" | colout . red faint.bold; return; }
         search_google "$search_term"
     done < <(csvgrep -c 2 -m 'y' -i "$search_terms_file" | csvcut -c 1 | csvformat -U 1 | tail -n +2)
 }
@@ -174,7 +174,7 @@ function search_google {
         if grep -qF 'rateLimitExceeded' <<< "$page_results"; then
             # Break loop if second key is also rate limited
             [[ "$google_search_id" == "$google_search_id_2" ]] && { rate_limited=true; break; }
-            printf "\nGoogle Search rate limited. Switching API keys.\n" | colout . white faint
+            printf "\nGoogle Search rate limited. Switching API keys.\n" | colout . white faint.bold
             google_search_api_key="$google_search_api_key_2" && google_search_id="$google_search_id_2"
             continue  # Continue to next page (current rate limited page is not repeated)
         fi
@@ -294,10 +294,10 @@ function process_source {
 
 function build {
     # Exit if no new domains to add (-s does not seem to work well here)
-    ! grep -q '[[:alnum:]]' retrieved_domains.tmp && { printf "\nNo new domains to add.\n" | calout . green; exit 0; }
+    ! grep -q '[[:alnum:]]' retrieved_domains.tmp && { printf "\nNo new domains to add.\n" | calout . white faint.bold; exit 0; }
     format_list retrieved_domains.tmp && format_list "$raw_file"
 
-    [[ -f in_toplist.tmp ]] || [[ -f invalid_entries.tmp ]] && printf "\nEntries requiring manual review:\n" | colout . white faint
+    [[ -f in_toplist.tmp ]] || [[ -f invalid_entries.tmp ]] && printf "\nEntries requiring manual review:\n" | colout . white faint.bold
     # Print domains found in toplist
     if [[ -f in_toplist.tmp ]]; then
         format_list in_toplist.tmp
@@ -324,7 +324,7 @@ function build {
     format_list "$raw_file"
     log_event "$(<retrieved_domains.tmp)" "new_domain" "retrieval"
     count_after=$(wc -l < "$raw_file")
-    printf "\nAdded new domains to blocklist.\n" | colout . white faint
+    printf "\nAdded new domains to blocklist.\n" | colout . white faint.bold
     printf "Before: %s  Added: %s  After: %s\n" "$count_before" "$((count_after - count_before))" "$count_after"
 
     # Mark sources as saved in the source log file
@@ -355,7 +355,7 @@ function log_source {
         'BEGIN {print time","source","search_term","raw","final","whitelist","dead","redundant","parked","toplist_count","toplist_domains","queries","rate_limited",no"}' >> "$source_log"
     [[ "$source" == 'Google Search' ]] && item="$search_term" || item="$source"
     excluded_count=$((dead_count + redundant_count + parked_count))
-    printf "\nSource: %s\n"  "$item" | colout . white faint
+    printf "\nSource: %s\n"  "$item" | colout . white faint.bold
     printf "Raw:%4s  Final:%4s  Whitelisted:%4s  Excluded:%4s  Toplist:%4s\n" "$unfiltered_count" "$filtered_count" "$total_whitelisted_count" "$excluded_count" "$toplist_count"
     printf "%s\n" "----------------------------------------------------------------------"
 }
