9adfa23d227b7d037b8ded4b81a12f08022daede
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-01 15:25:47 +0800

    Fix

--- functions/check_parked.sh
+++ functions/check_parked.sh
@@ -31,7 +31,8 @@ remove_parked_domains() {
     retrieve_parked "$RAW" || return
 
     # Remove parked domains from raw file
-    comm -23 "$RAW" parked_domains.tmp > raw.tmp && mv raw.tmp "$RAW"
+    comm -23 "$RAW" <(sort parked_domains.tmp) > raw.tmp
+    mv raw.tmp "$RAW"
 
     log_event "$(<parked_domains.tmp)" parked raw
 }
