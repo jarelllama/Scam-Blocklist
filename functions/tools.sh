63482693656e7fb975456766b04d2872078e0732
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-02 11:49:49 +0800

    Update

--- functions/tools.sh
+++ functions/tools.sh
@@ -3,7 +3,7 @@
 # Tools.sh is a shell wrapper intended to store commonly used functions.
 
 # Function 'format' is called to standardize the format of a file.
-# $1: file to be formatted
+#   $1: file to be formatted
 format() {
     file="$1"
     [[ ! -f "$file" ]] && return
