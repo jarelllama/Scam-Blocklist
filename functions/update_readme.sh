07ed9da76b0324855d995acb531dbec12334564f
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-03 19:37:08 +0800

    Update readme

--- functions/update_readme.sh
+++ functions/update_readme.sh
@@ -113,9 +113,9 @@ Domains in NRD feed: $(awk "BEGIN { printf \"%.2fM\", (( $(wc -l < lists/wildcar
 
 ### Other sources
 
-The other sources, active or inactive, can be viewed here: [SOURCES.md](https://github.com/jarelllama/Scam-Blocklist/blob/main/SOURCES.md).
+All sources used are recorded here: [SOURCES.md](https://github.com/jarelllama/Scam-Blocklist/blob/main/SOURCES.md).
 
-The full domain retrieval process for all sources can be viewed in the repository's code.
+The domain retrieval process for all sources can be viewed in the repository's code.
 
 ## Filtering process
 - The domains collated from all sources are filtered against a whitelist (scam reporting sites, forums, vetted stores, etc.)
