4f4abc8160cdfa216f1c06979d674ab1204d7dbe
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-09 10:57:53 +0800

    Fix

--- functions/check_parked.sh
+++ functions/check_parked.sh
@@ -45,8 +45,9 @@ check_parked() {
 
     # Strip subdomains from parked domains
     while read -r subdomain; do
-        sed "s/^${subdomain}\.//" parked.tmp | sort -u -o parked.tmp
+        sed -i "s/^${subdomain}\.//" parked.tmp
     done < "$SUBDOMAINS_TO_REMOVE"
+    sort -u parked.tmp -o parked.tmp
 
     remove_parked_from "$RAW"
     remove_parked_from "$RAW_LIGHT"
@@ -78,7 +79,7 @@ check_unparked() {
     # Add unparked domains to raw file
     # Note unparked subdomains are added back too and will be processed by the
     # validation check outside of this script
-    sort -u unparked.tmp "$RAW" -o "    $RAW"
+    sort -u unparked.tmp "$RAW" -o "$RAW"
 
     log_event "$(<unparked.tmp)" unparked parked_domains_file
 }
