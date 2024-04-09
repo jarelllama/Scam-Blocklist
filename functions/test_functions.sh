707b641e7ea5ffd230497bd1f8a2cca8a573d22a
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-10 00:29:01 +0800

    Fix

--- functions/test_functions.sh
+++ functions/test_functions.sh
@@ -465,7 +465,7 @@ test_source_log() {
     printf "source-log-test.com\n" >> data/pending/domains_petscams.com.tmp
     # EXPECTED OUTPUT
     printf "source-log-test.com\n" >> out_raw.txt
-    printf ",petscams.com,,1,1,0,0,0,0,0,,saved" >> out_source_log.txt
+    printf ",petscams.com,,1,1,0,0,0,0,,saved" >> out_source_log.txt
 }
 
 # TEST: exclusion of specific sources from light version
