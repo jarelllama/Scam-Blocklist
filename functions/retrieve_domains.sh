32d2a34a4b15e9d88b380f1378259f88ba7b748a
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-13 00:00:09 +0800

    Test

--- functions/retrieve_domains.sh
+++ functions/retrieve_domains.sh
@@ -32,17 +32,17 @@ source() {
 
     mkdir -p data/pending
 
-    source_manual
-    source_aa419
+    #source_manual
+    #source_aa419
     #source_dfpi  # Deactivated
     source_dnstwist
-    source_guntab
-    source_petscams
+    #source_guntab
+    #source_petscams
     source_regex
-    source_scamdirectory
-    source_scamadviser
-    source_stopgunscams
-    source_google_search
+    #source_scamdirectory
+    #source_scamadviser
+    #source_stopgunscams
+    #source_google_search
 }
 
 # Function 'filter' logs the given entries and removes them from the results
