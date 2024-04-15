cae8e39ac433013f2a5ba677d53969bad680fff0
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-15 20:59:04 +0800

    Add iam-py-test/blocklist_stats graph

--- functions/update_readme.sh
+++ functions/update_readme.sh
@@ -50,9 +50,10 @@ $(print_stats)
 All data retrieved are publicly available and can be viewed from their respective [sources](https://github.com/jarelllama/Scam-Blocklist/blob/main/SOURCES.md).
 
 <details>
-<summary>Domains over time</summary>
+<summary>Domains over time (days)</summary>
 
-![Graph](https://raw.githubusercontent.com/iam-py-test/blocklist_stats/main/stats/Jarelllamas_Scam_Blocklist.png)
+![Domains over time](https://raw.githubusercontent.com/iam-py-test/blocklist_stats/main/stats/Jarelllamas_Scam_Blocklist.png)
+
 Courtesy of iam-py-test/blocklist_stats.
 </details>
 
