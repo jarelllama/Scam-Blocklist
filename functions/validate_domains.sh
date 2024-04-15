3f8659e63ed2ac3ce52b4a035a5c10164f37a297
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-15 20:28:07 +0800

    Update

--- functions/validate_domains.sh
+++ functions/validate_domains.sh
@@ -125,14 +125,13 @@ validate() {
     printf "\n\e[1mProblematic domains (%s):\e[0m\n" "$(wc -l < filter_log.tmp)"
     sed 's/(toplist)/& - \o033[31mmanual verification required\o033[0m/' filter_log.tmp
 
-    # Remove subdomains because the notifications got annoying
+    # Do not notify for subdomains (the notifications got annoying)
     grep -vF 'subdomain' filter_log.tmp > temp
     mv temp filter_log.tmp
 
     [[ ! -s filter_log.tmp ]] && return
 
     # Call shell wrapper to send telegram notification
-    # (excluding subdomains because the notifications got annoying)
     $FUNCTION --send-telegram \
         "Problematic domains found during validation check:\n$(<filter_log.tmp)"
 
