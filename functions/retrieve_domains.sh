b4cd155f46b15840a1ec1b43cef112849dc4dabe
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-16 09:48:18 +0800

    Test

--- functions/retrieve_domains.sh
+++ functions/retrieve_domains.sh
@@ -42,16 +42,7 @@ source() {
     & { [[ "$USE_EXISTING" != true ]] && download_nrd_feed; }
     wait
 
-    source_manual
-    source_aa419
-    source_dnstwist
-    source_guntab
-    source_petscams
-    source_regex
-    source_scamdirectory
     source_scamadviser
-    source_stopgunscams
-    source_google_search
 }
 
 # Function 'filter' logs the given entries and removes them from the results
