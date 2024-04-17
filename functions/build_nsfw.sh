c1dd545c50ceb442f133c54b595f38cda38897d0
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-17 12:37:03 +0800

    Proof of concept for #308

---
+++ functions/build_nsfw.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Builds the NSFW blocklist. The build process is entirely self-contained in
+# this one script.
+
+readonly FUNCTION='bash functions/tools.sh'
+readonly BLOCKLIST='lists/adblock/nsfw.txt'
+
+# Function 'build' retrieves domains from the Tranco toplist, adds them to the
+# raw file, formats it, and removes dead domains.
+build() {
+    # Patterns to match for
+    terms=(
+        porn
+        xxx
+        spankbang
+        xhamster
+        xvideos
+        onlyfans
+        fansly
+        hentai
+    )
+
+    # Format raw file
+    grep -vF '!' "$BLOCKLIST" > raw.tmp
+    sed -i 's/||//; s/\^//' raw.tmp
+
+    # Get new domains from toplist
+    for term in "${terms[@]}"; do
+        grep "$term" toplist.tmp >> domains.tmp
+    done
+
+    # Remove domains already in raw file
+    comm -23 domains.tmp raw.tmp > temp
+    mv temp domains.tmp
+
+    # Log new domains
+    $FUNCTION --log-domains domains.tmp nsfw toplist
+
+    # Add new domains to raw file
+    sort -u domains.tmp raw.tmp -o raw.tmp
+
+    # Format to Adblock Plus syntax
+    sed -i 's/.*/||&^/' raw.tmp
+
+    # Remove dead domains
+    dead-domains-linter -a -i raw.tmp > /dev/null
+}
+
+# Function 'deploy' builds the blocklist in Adblock Plus syntax.
+deploy() {
+    cat << EOF > "$BLOCKLIST"
+[Adblock Plus]
+! Title: Jarelllama's NSFW Blocklist
+! Description: Blocklist for NSFW content automatically retrieved daily.
+! Homepage: https://github.com/jarelllama/Scam-Blocklist
+! License: https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/LICENSE.md
+! Version: $(date -u +"%m.%d.%H%M%S.%Y")
+! Expires: 1 day
+! Last modified: $(date -u)
+! Syntax: Adblock Plus
+! Number of entries: $(wc -l < raw.tmp)
+!
+EOF
+    cat raw.tmp >> "$BLOCKLIST"
+}
+
+# Entry point
+
+trap 'find . -maxdepth 1 -type f -name "*.tmp" -delete' EXIT
+
+$FUNCTION --format-all
+
+# Install AdGuard's Dead Domains Linter
+if ! command -v dead-domains-linter &> /dev/null; then
+    npm install -g @adguard/dead-domains-linter > /dev/null
+fi
+
+# Download the Tranco toplist
+$FUNCTION --download-toplist
+
+build
+
+deploy
