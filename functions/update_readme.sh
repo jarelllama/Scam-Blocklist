52756efd3166393e40da5e1b235ae30e00a94ac7
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-18 02:44:55 +0800

    Update readme

--- functions/update_readme.sh
+++ functions/update_readme.sh
@@ -89,6 +89,9 @@ Created from requests, a blocklist for NSFW domains is available in Adblock Plus
 <li>Note that parked domains are not checked for in this blocklist</li>
 </ul>
 Total domains: $(wc -l <<< "$(grep '||' lists/adblock/nsfw.txt)")
+<br>
+<br>
+This blocklist does not just include adult videos, but also NSFW content of the artistic variety (rule34, illustrations, etc).
 </details>
 
 ## Sources
