d00ce4864d0a5a16414b0954d5c39479fd811720
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-18 18:17:24 +0800

    Update dead/parked cache limits

--- functions/check_parked.sh
+++ functions/check_parked.sh
@@ -182,7 +182,7 @@ cleanup() {
     find . -maxdepth 1 -type f -name "x??" -delete
 
     # Call shell wrapper to prune old entries from parked domains file
-    $FUNCTION --prune-lines "$PARKED_DOMAINS" 7000
+    $FUNCTION --prune-lines "$PARKED_DOMAINS" 8000
 }
 
 # Entry point
