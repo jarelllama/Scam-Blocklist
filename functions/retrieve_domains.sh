11281eb5a3041a1bdfa558b9b2ec203f4a83a896
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-20 01:50:04 +0800

    retrieve_domains.sh: update comments

--- functions/retrieve_domains.sh
+++ functions/retrieve_domains.sh
@@ -602,7 +602,7 @@ source_phishstats() {
     local url='https://phishstats.info/phish_score.csv'
     # Get only URLs with no subdirectories, exclude IP addresses and extract
     # domains
-    # We can omit the -o argument for grep since each entry is on its own line.
+    # -o for grep can be omitted since each entry is on its own line
     wget -qO - "$url" | awk -F ',' '{print $3}' \
         | grep -E '^"?https?://[[:alnum:].-]+\.[[:alnum:]-]*[a-z]{2,}[[:alnum:]-]*."?$' \
         | sed 's/"//g' | awk -F '/' '{print $3}' | sort -u -o "$results_file"
