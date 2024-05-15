5b103a6d0201b206743c1af38e025defedd8b71a
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-05-15 04:12:15 +0000

    Workflow: change shceduled time to 7pm

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -8,7 +8,7 @@ update_readme() {
 
 ${BLOCKLIST_DESCRIPTION}
 
-The [automated retrieval](https://github.com/jarelllama/Scam-Blocklist/actions/workflows/build_deploy.yml) is done daily at 10:00 AM UTC.
+The [automated retrieval](https://github.com/jarelllama/Scam-Blocklist/actions/workflows/build_deploy.yml) is done daily at 19:00 UTC.
 
 This blocklist aims to be an alternative to blocking all newly registered domains (NRDs) seeing how many, but not all, NRDs are malicious. A variety of sources are integrated to detect new malicious domains within a short time span of their registration date.
 
