e396045cc7ac8860b5675dc6301f5af4b9d9e223
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-11-26 17:14:27 +0800

    Increase parked and dead domains log to 12000

--- scripts/check_parked.sh
+++ scripts/check_parked.sh
@@ -182,7 +182,7 @@ cleanup() {
     find . -maxdepth 1 -type f -name "x??" -delete
 
     # Call shell wrapper to prune old entries from parked domains file
-    $FUNCTION --prune-lines "$PARKED_DOMAINS" 10000
+    $FUNCTION --prune-lines "$PARKED_DOMAINS" 12000
 }
 
 # Entry point
