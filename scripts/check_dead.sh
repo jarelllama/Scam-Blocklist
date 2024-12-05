78e2a53966b12a10597510b61962877fd17b7cd9
J <91372088+jarelllama@users.noreply.github.com>
2024-12-06 00:19:51 +0800

    check_dead.sh: increase cache to 15000 domains

--- scripts/check_dead.sh
+++ scripts/check_dead.sh
@@ -120,7 +120,7 @@ cleanup() {
     find . -maxdepth 1 -type f -name "*.tmp" -delete
 
     # Call shell wrapper to prune old entries from dead domains file
-    $FUNCTION --prune-lines "$DEAD_DOMAINS" 12000
+    $FUNCTION --prune-lines "$DEAD_DOMAINS" 15000
 }
 
 # Entry point
