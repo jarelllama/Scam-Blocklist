f4cf75d7ea8c3a22c214d113f4dd95755438ca36
J <91372088+jarelllama@users.noreply.github.com>
2024-12-16 10:22:13 +0800

    tests: debug

--- scripts/test_functions.sh
+++ scripts/test_functions.sh
@@ -179,7 +179,7 @@ TEST_DEAD_CHECK() {
     grep -vxF 'www.google.com' out_raw.txt > out_raw_light.txt
 
     # Run script
-    cat out_dead.txt # DEBUG
+    cat "$DEAD_DOMAINS_FILE" # DEBUG
     run_script check_dead.sh checkalive
     run_script check_dead.sh part1
     run_script check_dead.sh part2
