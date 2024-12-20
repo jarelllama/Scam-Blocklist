906b5dd9f50fc9baf48fa8ce37d7b6539df7e9d2
J <91372088+jarelllama@users.noreply.github.com>
2024-12-20 17:55:11 +0800

    retrieval: remove redundant variable

--- scripts/retrieve_domains.sh
+++ scripts/retrieve_domains.sh
@@ -230,9 +230,6 @@ process_source() {
 # Function 'build' appends the filtered domains into the raw files and presents
 # some basic numbers to the user.
 build() {
-    # For telegram message
-    workflow_url='https://github.com/jarelllama/Scam-Blocklist/actions/workflows/auto_retrieve.yml'
-
     if [[ -f manual_review.tmp ]]; then
         # Print domains requiring manual review
         printf "\n\e[1mEntries requiring manual review:\e[0m\n"
