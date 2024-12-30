ee10a877017efecc93ee1e2560e4ece1cf49129b
J <91372088+jarelllama@users.noreply.github.com>
2024-12-30 16:14:27 +0800

    Update readme

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -11,11 +11,9 @@ update_readme() {
 
 ${BLOCKLIST_DESCRIPTION}
 
-Since the project began, the blocklist has expanded to include not only scam websites but also malware domains.
+This blocklist aims to detect new malicious domains within a short period of their registration date. Since the project began, the blocklist has expanded to include not only scam websites but also malware domains.
 
-This blocklist aims to be an alternative to blocking all newly registered domains (NRDs) seeing how many, but not all, NRDs are malicious. This is done by detecting new malicious domains within a short period of their registration date.
-
-For blocking all NRDs, use [xRuffKez's NRD Lists](https://github.com/xRuffKez/NRD).
+For extended protection, use [xRuffKez's NRD Lists](https://github.com/xRuffKez/NRD) to block all newly registered domains (NRDs), and [Hagezi's Threat Intelligence Feed](https://github.com/hagezi/dns-blocklists?tab=readme-ov-file#tif) (full version) which includes this blocklist .
 
 Sources include:
 
@@ -35,8 +33,6 @@ The automated retrieval is done daily at 16:00 UTC.
 | --- | --- |
 | [Adblock Plus](https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/lists/adblock/scams.txt) | \|\|scam.com^ |
 | [Wildcard Domains](https://raw.githubusercontent.com/jarelllama/Scam-Blocklist/main/lists/wildcard_domains/scams.txt) | scam.com |
-
-This blocklist is integrated into [Hagezi's Threat Intelligence Feed](https://github.com/hagezi/dns-blocklists?tab=readme-ov-file#tif) (full version). For extended protection, please use that list instead.
 
 ## Statistics
 
