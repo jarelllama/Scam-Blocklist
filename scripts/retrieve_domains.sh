90d759a68eef12df1d0c9dd6ad696dd381af589c
J <91372088+jarelllama@users.noreply.github.com>
2025-01-04 11:39:09 +0800

    retrieve: quietly remove needrestart

--- scripts/retrieve_domains.sh
+++ scripts/retrieve_domains.sh
@@ -897,8 +897,8 @@ source_vzhh() {
 trap cleanup EXIT
 
 # Uninstall needrestart
-# sudo is needed here
-sudo apt-get remove -qq needrestart
+# sudo is needed here (note -qq does not seem to work here)
+sudo apt-get remove needrestart > /dev/null
 
 $FUNCTION --format-all
 
