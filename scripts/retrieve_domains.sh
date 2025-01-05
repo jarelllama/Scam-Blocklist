2b8321147013a9d6e15c3bac37e11c8687f29628
J <91372088+jarelllama@users.noreply.github.com>
2025-01-05 15:14:43 +0800

    test pcrisk and safelyweb sources

--- scripts/retrieve_domains.sh
+++ scripts/retrieve_domains.sh
@@ -30,29 +30,11 @@ readonly DOMAIN_DASH_REGEX='[[:alnum:].-]+-[[:alnum:]-]+'
 readonly DOMAIN_REGEX='[[:alnum:]][[:alnum:].-]*[[:alnum:]]\.[[:alnum:]-]*[a-z]{2,}[[:alnum:]-]*'
 
 readonly -a SOURCES=(
-    source_165antifraud
-    source_aa419
-    source_coi.gov.cz
-    source_cybersquatting
-    source_dga_detector
-    source_emerging_threats
-    source_fakewebshoplisthun
-    source_jeroengui
-    source_jeroengui_nrd
-    source_gridinsoft
-    source_manual
+
     source_pcrisk
-    source_phishstats
-    source_phishstats_nrd
-    source_puppyscams
-    source_regex
+
     source_safelyweb
-    source_scamadviser
-    source_scamdirectory
-    source_stopgunscams
-    source_viriback_tracker
-    source_vzhh
-    source_google_search
+
 )
 
 # Function 'source' calls on the respective functions of each source to
@@ -782,7 +764,7 @@ source_manual() {
 
 source_pcrisk() {
     # Last checked: 05/01/25
-    source='PCrisk.com'
+    source='PCrisk'
     results_file="data/pending/domains_${source}.tmp"
 
     [[ "$USE_EXISTING" == true ]] && { process_source; return; }
