298439bfe79e12865491ecc77f19de0525300672
J <91372088+jarelllama@users.noreply.github.com>
2025-01-14 11:25:57 +0800

    build_list.sh: refactor script

--- scripts/build_lists.sh
+++ scripts/build_lists.sh
@@ -15,27 +15,25 @@ main() {
         npm install -g @adguard/hostlist-compiler > /dev/null
     fi
 
-    # Loop through the full and light blocklist versions
-    for VERSION in '' 'LIGHT VERSION'; do
-        if [[ "$VERSION" == '' ]]; then
-            list='scams.txt'
-            source="$RAW"
-        else
-            list='scams_light.txt'
-            source="$RAW_LIGHT"
-        fi
-
-        build
-    done
+    build '' "$RAW" 'scams.txt'
+    build 'LIGHT VERSION' "$RAW_LIGHT" 'scams_light.txt'
 }
 
 # Function 'build' removes redundant entries from the raw files and compiles
 # them into the various blocklist formats.
+# Input:
+#   $1: blocklist version
+#   $2: raw file
+#   $3: output file
 build() {
-    # Append wildcards to the raw file to optimize the nubmer of entries.
+    local version="$1"
+    local raw_file="$2"
+    local output_file="$3"
+
+    # Append wildcards to the raw file to optimize the number of entries.
     # The wildcards are not saved to the raw file as some of them do not
     # resolve and would be removed by the dead check.
-    sort -u "$WILDCARDS" "$source" -o source.tmp
+    sort -u "$WILDCARDS" "$raw_file" -o source.tmp
 
     # Compile blocklist. See the list of transformations here:
     # https://github.com/AdguardTeam/HostlistCompiler
@@ -46,31 +44,32 @@ build() {
     sed -i '/!/d' compiled.tmp
 
     # Build Adblock Plus format
-    printf "[Adblock Plus]\n" > "${ADBLOCK}/${list}"
-    append_header '!' "$ADBLOCK" "Adblock Plus"
-    cat compiled.tmp >> "${ADBLOCK}/${list}"
+    local dir="$ADBLOCK"
+    printf "[Adblock Plus]\n" > "${dir}/${output_file}"
+    append_header '!' 'Adblock Plus'
+    cat compiled.tmp >> "${dir}/${output_file}"
 
     # Build Wildcard Domains format
-    : > "${DOMAINS}/${list}"
-    append_header '#' "$DOMAINS" "Wildcard Domains"
-    sed 's/[|\^]//g' compiled.tmp >> "${DOMAINS}/${list}"
+    local dir="$DOMAINS"
+    : > "${dir}/${output_file}"
+    append_header '#' 'Wildcard Domains'
+    sed 's/[|\^]//g' compiled.tmp >> "${dir}/${output_file}"
 }
 
 # Function 'append_header' appends the header onto the blocklist.
 # Input:
 #   $1: comment character to use
-#   $2: directory of the blocklist to append to
-#   $3: syntax of the blocklist
+#   $2: syntax of the blocklist
 append_header() {
-    cat << EOF >> "$2/${list}"
-${1} Title: Jarelllama's Scam Blocklist ${VERSION}
+    cat << EOF >> "${dir}/${output_file}"
+${1} Title: Jarelllama's Scam Blocklist ${version}
 ${1} Description: ${BLOCKLIST_DESCRIPTION}
 ${1} Homepage: https://github.com/jarelllama/Scam-Blocklist
 ${1} License: https://github.com/jarelllama/Scam-Blocklist/blob/main/LICENSE.md
 ${1} Version: $(date -u +"%m.%d.%H%M%S.%Y")
 ${1} Expires: 12 hours
 ${1} Last modified: $(date -u)
-${1} Syntax: ${3}
+${1} Syntax: ${2}
 ${1} Number of entries: $(wc -l < compiled.tmp)
 ${1}
 EOF
