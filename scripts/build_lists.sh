a9596b49b9a468e16e358c1f2380370e2fb58f30
J <91372088+jarelllama@users.noreply.github.com>
2025-01-21 23:23:38 +0800

    build: update comments and use set -e

--- scripts/build_lists.sh
+++ scripts/build_lists.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Builds the various formats of blocklists from the raw files.
+# Build the various formats of blocklists from the raw files.
 
 readonly FUNCTION='bash scripts/tools.sh'
 readonly RAW='data/raw.txt'
@@ -19,16 +19,17 @@ main() {
     build 'LIGHT VERSION' "$RAW_LIGHT" 'scams_light.txt'
 }
 
-# Function 'build' removes redundant entries from the raw files and compiles
-# them into the various blocklist formats.
+# Remove redundant entries from the raw files and compile them into the various
+# blocklist formats.
 # Input:
-#   $1: blocklist version
+#   $1: blocklist version name
 #   $2: raw file
 #   $3: output file
 build() {
     local version="$1"
     local raw_file="$2"
     local output_file="$3"
+    local dir
 
     # Append wildcards to the raw file to optimize the number of entries.
     # The wildcards are not saved to the raw file as some of them do not
@@ -44,19 +45,19 @@ build() {
     sed -i '/!/d' compiled.tmp
 
     # Build Adblock Plus format
-    local dir="$ADBLOCK"
+    dir="$ADBLOCK"
     printf "[Adblock Plus]\n" > "${dir}/${output_file}"
     append_header '!' 'Adblock Plus'
     cat compiled.tmp >> "${dir}/${output_file}"
 
     # Build Wildcard Domains format
-    local dir="$DOMAINS"
+    dir="$DOMAINS"
     : > "${dir}/${output_file}"
     append_header '#' 'Wildcard Domains'
     sed 's/[|\^]//g' compiled.tmp >> "${dir}/${output_file}"
 }
 
-# Function 'append_header' appends the header onto the blocklist.
+# Append the header onto the blocklist.
 # Input:
 #   $1: comment character to use
 #   $2: syntax of the blocklist
@@ -76,6 +77,8 @@ EOF
 }
 
 # Entry point
+
+set -e
 
 trap 'find . -maxdepth 1 -type f -name "*.tmp" -delete' EXIT
 
