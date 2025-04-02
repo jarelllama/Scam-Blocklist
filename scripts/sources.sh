537c8f0ce089e019ecf38e91896c5085321d70b2
J <91372088+jarelllama@users.noreply.github.com>
2025-04-02 19:03:26 +0800

    update sources.sh

--- scripts/sources.sh
+++ scripts/sources.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Run a source function to retrieve the results from the source.
+# Run a source function to retrieve results. The results are usually from the
+# last 3 months.
 # Input:
 #   $1: source function to run
 # Output:
@@ -21,16 +22,17 @@ CURL() {
     curl -sSLZ --retry 2 --retry-all-errors "${1:-$URL}"
 }
 
+# Note that for sources with multiple pages to scrape, using mawk to match a
+# specific line may cause some pages to appear broken.
+
 165antifraud() {
-    # Last checked: 29/03/25
     URL='https://165.npa.gov.tw/api/article/subclass/3'
-    CURL \
-        | jq --arg year "$(date +%Y)" '.[] | select(.publishDate | contains($year)) | .content' \
-        | grep -Po "\\\">\K${DOMAIN_REGEX}" > results.tmp
+    CURL | jq --arg year "$(date +%Y)" '
+        .[] | select(.publishDate | contains($year)) | .content' \
+        | grep -Po "\\\">(https?://)?\K${DOMAIN_REGEX}" > results.tmp
 }
 
 aa419() {
-    # Last checked: 29/03/25
     URL='https://api.aa419.org/fakesites'
     curl -sSL --retry 2 --retry-all-errors -H "Auth-API-Id:${AA419_API_ID}" \
         "${URL}/0/500?fields=Domain" \
@@ -38,34 +40,29 @@ aa419() {
 }
 
 behindmlm() {
-    # Last checked: 29/03/25
     URL='https://behindmlm.com'
-    CURL "${URL}/page/[1-15]" \
+    CURL "${URL}/page/[1-25]" \
         | grep -iPo "(&#8220;|<li>|; |: |and )\K${DOMAIN_REGEX}" > results.tmp
 }
 
 bugsfighter() {
-    # Last checked: 29/03/25
-    URL='https://www.bugsfighter.com/blog'
-    CURL "${URL}/page/[1-50]" | grep -iPo "remove \K${DOMAIN_REGEX}" \
+    URL='https://www.bugsfighter.com/mac-viruses'
+    CURL "${URL}/page/[1-75]" | grep -iPo "remove \K${DOMAIN_REGEX}" \
         > results.tmp
 }
 
 chainabuse() {
-    # Last checked: 03/03/25
     URL='https://raw.githubusercontent.com/jarelllama/Blocklist-Sources/refs/heads/main/chainabuse.txt'
     CURL > results.tmp
 }
 
 coi.gov.cz() {
-    # Last checked: 29/03/25
     URL='https://coi.gov.cz/pro-spotrebitele/rizikove-e-shopy'
     CURL | mawk '/<p class = "list_titles">/ { getline; getline; print }' \
-        | grep -Po "<span>(https?://)?\K$DOMAIN_REGEX" > results.tmp
+        | grep -Po "<span>(https?://)?\K${DOMAIN_REGEX}" > results.tmp
 }
 
 crypto_scam_tracker() {
-    # Last checked: 31/03/25
     URL='https://dfpi.ca.gov/consumers/crypto/crypto-scam-tracker'
     CURL | mawk '
         /"column-4"/ {
@@ -88,26 +85,23 @@ crypto_scam_tracker() {
 }
 
 dga_detector() {
-    # Last checked: 28/03/25
     URL='https://github.com/jarelllama/dga_detector/archive/refs/heads/master.zip'
-
-    # Install DGA Detector and dependencies
     CURL > dga_detector.zip
     unzip -q dga_detector.zip
     pip install -q tldextract
 
-    # Keep only non-Punycode NRDs with 16 or more characters (including period
-    # and TLD)
+    # Get non-Punycode NRDs with 16 or more characters including the period
+    # and TLD
     mawk 'length($0) >= 16 && !/xn--/' nrds.tmp > domains.tmp
 
     cd dga_detector-master
 
     # Set the detection threshold. DGA domains fall below this threshold.
-    # A lower threshold lowers the domain yield and reduces false positives.
+    # A lower threshold reduces false positives.
     sed -i "s/threshold = model_data\['thresh'\]/threshold = 0.0008/" \
         dga_detector.py
 
-    # Run DGA Detector on the remaining NRDs
+    # Run DGA Detector on the NRDs
     python3 dga_detector.py -f ../domains.tmp > /dev/null
 
     # Extract DGA domains from the JSON output
@@ -119,7 +113,6 @@ dga_detector() {
 }
 
 dnstwist_() {
-    # Last checked: 30/03/25
     local tlds results
 
     command -v dnstwist > /dev/null || pip install -q dnstwist
@@ -159,19 +152,18 @@ dnstwist_() {
 }
 
 emerging_threats() {
-    # Last checked: 28/03/25
     URL='https://rules.emergingthreats.net/open/suricata-5.0/emerging.rules.zip'
     CURL > rules.zip
-    unzip -q rules.zip -d rules
+    unzip -q rules.zip
 
     # Ignore rules with specific payload keywords. See here:
     # https://docs.suricata.io/en/suricata-6.0.0/rules/payload-keywords.html
     # Note 'endswith' is accepted as those rules tend to be wildcard matches of
     # root domains (leading periods are removed for those rules).
-    local RULE
-    for RULE in emerging-adware_pup emerging-coinminer emerging-exploit_kit \
+    local rule
+    for rule in emerging-adware_pup emerging-coinminer emerging-exploit_kit \
         emerging-malware emerging-mobile_malware emerging-phishing; do
-        cat "rules/rules/${RULE}.rules"
+        cat "rules/${rule}.rules"
     done | mawk '/dns[\.|_]query/ &&
         !/^#|content:!|startswith|offset|distance|within|pcre/' \
         | grep -Po "content:\"\.?\K${DOMAIN_REGEX}" > results.tmp
@@ -180,26 +172,40 @@ emerging_threats() {
 }
 
 fakewebshoplisthun() {
-    # Last checked: 28/03/25
     URL='https://raw.githubusercontent.com/FakesiteListHUN/FakeWebshopListHUN/refs/heads/main/fakewebshoplist'
-    CURL | grep -Po "^(\|\|)?\K${DOMAIN_REGEX}(?=\^?$)" > results.tmp
+    CURL | grep -Po "^(\|\|)?\K${DOMAIN_REGEX}(?=(\^|/)?$)" > results.tmp
 }
 
 greatis() {
-    # Last checked: 28/03/25
     URL='https://greatis.com/unhackme/help/category/remove'
-    CURL "${URL}/page/[1-15]" | grep -iPo "remove \K${DOMAIN_REGEX}" \
+    CURL "${URL}/page/[1-25]" | grep -iPo "remove \K${DOMAIN_REGEX}" \
         > results.tmp
 }
 
 gridinsoft() {
-    # Last checked: 17/02/25
     URL='https://raw.githubusercontent.com/jarelllama/Blocklist-Sources/refs/heads/main/gridinsoft.txt'
     CURL > results.tmp
 }
 
+guntab() {
+    URL='https://www.guntab.com/scam-websites'
+    CURL | grep -Po " \K${DOMAIN_REGEX}$" > results.tmp
+}
+
+howtofix.guide() {
+    URL='https://howtofix.guide/category/browser-hijacker'
+    CURL "${URL}/page/[1-30]" | grep -Po "title=\"\K${DOMAIN_REGEX}" \
+        > results.tmp
+}
+
+howtoremove.guide() {
+    URL='https://howtoremove.guide/category/tips'
+    CURL "${URL}/page/[1-15]" | mawk '/>Tips<\/h1>/' \
+        | grep -Po "[A-Z0-9][-.]?${DOMAIN_REGEX}(?=[[:space:]]+([A-Z]|a ))" \
+        > results.tmp
+}
+
 jeroengui() {
-    # Last checked: 28/03/25
     URL='https://file.jeroengui.be'
     url_shorterners='https://raw.githubusercontent.com/hagezi/dns-blocklists/refs/heads/main/adblock/whitelist-urlshortener.txt'
 
@@ -208,6 +214,7 @@ jeroengui() {
         "${URL}/phishing/last_month.txt" \
         "${URL}/malware/last_month.txt" \
         "${URL}/scam/last_month.txt" \
+        "${URL}/web_shell/last_month.txt" \
         | grep -Po "^https?://\K${DOMAIN_REGEX}" \
         | grep -vF "$(CURL "$url_shorterners" \
         | grep -Po "\|\K${DOMAIN_REGEX}")" > results.tmp
@@ -218,34 +225,29 @@ jeroengui() {
 }
 
 jeroengui_nrds() {
-    # Last checked: 29/12/24
-    # Only includes domains found in the NRD feed for the light version
+    # Only include domains found in the NRD feed for the light version
     mv jeroengui_nrds.tmp results.tmp
 }
 
 malwarebytes() {
-    # Last checked: 28/03/25
     URL='https://www.malwarebytes.com/blog/detections'
     CURL | grep -Po ">\K${DOMAIN_REGEX}(?=</a>)" | mawk '!/[A-Z]/' \
         > results.tmp
 }
 
 malwareurl() {
-    # Last checked: 17/02/25
     URL='https://raw.githubusercontent.com/jarelllama/Blocklist-Sources/refs/heads/main/malwareurl.txt'
     CURL > results.tmp
 }
 
 pcrisk() {
-    # Last checked: 28/03/25
     URL='https://www.pcrisk.com/removal-guides'
-    CURL "${URL}?start=[0-50]0" \
+    CURL "${URL}?start=[0-70]0" \
         | mawk '/article class/ { getline; getline; getline; getline; print }' \
         | grep -Po ">\K${DOMAIN_REGEX}" > results.tmp
 }
 
 phishing_nrds() {
-    # Last checked: 30/03/25
     local pattern escaped_target regex
 
     # Loop through the phishing targets
@@ -279,49 +281,43 @@ phishing_nrds() {
 }
 
 phishstats() {
-    # Last checked: 30/03/25
     URL='https://phishstats.info/phish_score.csv'
     CURL | grep -Po ",\"https?://\K${DOMAIN_REGEX}" > results.tmp
 }
 
 podvodnabazaru.cz() {
-    # Last checked: 31/03/25
     URL='https://podvodnabazaru.cz/database/scam-eshop'
     CURL | mawk '/Podvodný eshop/ { getline; getline; print }' \
         | grep -Po "${DOMAIN_REGEX}" > results.tmp
 }
 
 puppyscams() {
-    # Last checked: 30/02/25
     URL='https://puppyscams.org'
-    CURL "${URL}/?page=[1-15]" | grep -Po " \K${DOMAIN_REGEX}(?=</h4></a>)" \
+    CURL "${URL}/?page=[1-30]" | grep -Po " \K${DOMAIN_REGEX}(?=</h4></a>)" \
         > results.tmp
 }
 
 safelyweb() {
-    # Last checked: 30/03/25
     URL='https://safelyweb.com/scams-database'
-    CURL "${URL}/?per_page=[1-30]" \
+    CURL "${URL}/?per_page=[1-100]" \
         | grep -iPo "suspicious website</div> <h2 class=\"title\">\K${DOMAIN_REGEX}" \
         > results.tmp
 }
 
 scamadviser() {
-    # Last checked: 30/03/25
     URL='https://www.scamadviser.com/articles'
-    CURL "${URL}?p=[1-15]" | grep -Po "[A-Z0-9][-.]?${DOMAIN_REGEX}" \
+    CURL "${URL}?p=[1-15]" | mawk '/title":"Scam Alerts","description/' \
+        | grep -Po "[A-Z0-9][-.]?${DOMAIN_REGEX}(?=[[:space:]]+([A-Z]|a ))" \
         > results.tmp
 }
 
 scamdirectory() {
-    # Last checked: 29/03/25
     URL='https://scam.directory/category'
     # head -n causes grep broken pipe error
     CURL | grep -Po "<span>\K${DOMAIN_REGEX}(?=<br>)" > results.tmp
 }
 
 scamminder() {
-    # Last checked: 29/03/25
     URL='https://scamminder.com/websites'
     # There are about 150 new pages daily
     CURL "${URL}/page/[1-1000]" \
@@ -330,13 +326,11 @@ scamminder() {
 }
 
 scamscavenger() {
-    # Last checked: 10/03/25
     URL='https://raw.githubusercontent.com/jarelllama/Blocklist-Sources/refs/heads/main/scamscavenger.txt'
     CURL > results.tmp
 }
 
 scamtracker() {
-    # Last checked: 29/03/25
     local -a review_urls
 
     URL='https://scam-tracker.net/category/crypto-scams'
@@ -352,7 +346,6 @@ scamtracker() {
 }
 
 unit42() {
-    # Last checked: 29/03/25
     URL='https://github.com/PaloAltoNetworks/Unit42-timely-threat-intel/archive/refs/heads/main.zip'
     CURL > unit42.zip
     unzip -q unit42.zip -d unit42
@@ -364,12 +357,9 @@ unit42() {
 }
 
 urlcrazy() {
-    # Last checked: 30/03/25
     local results
 
     URL='https://github.com/urbanadventurer/urlcrazy/archive/refs/heads/master.zip'
-
-    # Install URLCrazy and dependencies
     CURL > urlcrazy.zip
     unzip -q urlcrazy.zip
     command -v ruby > /dev/null || apt-get install -qq ruby ruby-dev
@@ -405,25 +395,28 @@ urlcrazy() {
 }
 
 viriback_tracker() {
-    # Last checked: 29/03/25
     URL='https://tracker.viriback.com/dump.php'
     CURL | mawk -v year="$(date +%Y)" -F ',' '$4 ~ year { print $2 }' \
-        | grep -Po "^(https?://)?\K${DOMAIN_REGEX}" > results.tmp
+        | grep -Po "^(https?://)\K${DOMAIN_REGEX}" > results.tmp
 }
 
 vzhh.de() {
-    # Last checked: 29/03/25
     URL='https://www.vzhh.de/themen/einkauf-reise-freizeit/einkauf-online-shopping/fake-shop-liste-wenn-guenstig-richtig-teuer-wird'
     CURL | mawk '/Shops in alphabetischer Reihenfolge/' \
         | grep -Po "${DOMAIN_REGEX}" > results.tmp
 }
 
 wipersoft() {
-    # Last checked: 29/03/25
     URL='https://www.wipersoft.com/blog'
+    # Hangs when too many pages are requested
     CURL "${URL}/page/[1-25]" \
         | mawk '/<div class="post-content">/ { getline; print }' \
         | grep -Po "${DOMAIN_REGEX}" > results.tmp
+}
+
+malwaretips() {
+    # TODO
+    URL=
 }
 
 # Entry point
